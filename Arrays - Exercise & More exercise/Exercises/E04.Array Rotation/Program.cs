d0807a19169e1c75f4aedce62a73496417ea9075
Polinadrumeva <polina9230@gmail.com>
2022-04-02 12:50:53 +0300

    Add exercise

---
+++ Arrays - Exercise & More exercise/Exercises/E04.Array Rotation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Array 
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var arr = Console.ReadLine().Split().ToArray();
+            int n = int.Parse(Console.ReadLine());
+            for (int j = 0; j < n; j++)
+            {
+                var end = arr[0];
+                for (var i = 0; i < arr.Length - 1; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+                arr[arr.Length - 1] = end;
+            }
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
---
+++ Arrays - Exercise & More exercise/More exercise/ME03. Recursive Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ME04.RecursiveFibonacci
+{
+    internal class Program
+    {
+        public static int Fib(int num)
+        {
+            if (num <= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fib(num - 1) + Fib(num - 2);
+
+            }
+
+        }
+
+        public static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fib(num));
+        }
+    }
+
+}
