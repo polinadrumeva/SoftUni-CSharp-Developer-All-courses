fb0b58dbd5c56fc219d5ef3c28ac0375772dba9b
Polinadrumeva <polina9230@gmail.com>
2022-04-03 15:42:41 +0300

    Add

---
+++ Arrays - Exercise & More exercise/Exercises/E07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace E07._Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int count = 0;
+            int lastCount = 0;
+            int repeatDigit = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int k = i+1; k < numbers.Length; k++)
+                {
+                    if (numbers[i] == numbers[k])
+                    {
+                        count++;
+
+                        if (count > lastCount)
+                        {
+                            lastCount = count;
+                            repeatDigit = numbers[i];
+
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                   
+                }
+                count = 0;
+            }
+
+            int[] repeatArray = new int[lastCount+1];
+            for (int i = 0; i < repeatArray.Length; i++)
+            {
+                repeatArray[i] = repeatDigit;
+            }
+
+            Console.WriteLine(String.Join(" ", repeatArray));
+        }
+    }
+}
---
+++ Arrays - Exercise & More exercise/Exercises/E08. Magic Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace E08._Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int givenNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int k = i + 1; k < numbers.Length; k++)
+                {
+                    if (numbers[i] + numbers[k] == givenNumber)
+                    {
+                        Console.WriteLine($"{numbers[i]} {numbers[k]}");
+                    }
+                }
+
+            }
+        }
+    }
+}
