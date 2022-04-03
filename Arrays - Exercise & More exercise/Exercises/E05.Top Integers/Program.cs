0b0d5041c3eb332dc7adece0744ee627a2e910fe
Polinadrumeva <polina9230@gmail.com>
2022-04-03 13:19:38 +0300

    Add exercise

---
+++ Arrays - Exercise & More exercise/Exercises/E05.Top Integers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace TopIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                bool isTopInteger = true;
+                for (int k = i + 1; k < numbers.Length; k++)
+                {
+                    if (numbers[i] <= numbers[k])
+                    {
+                        isTopInteger = false;
+                    }
+                }
+
+                if (isTopInteger)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+        }
+    }
+}
