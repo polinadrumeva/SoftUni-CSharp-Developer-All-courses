68646386e4a7bb1e07a86a3b5f3d858d13045ae0
Polinadrumeva <polina9230@gmail.com>
2022-04-04 21:36:39 +0300

    Add more exercise

---
+++ Arrays - Exercise & More exercise/More exercise/ME04. Fold and Sum/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace ME04._Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] firstNumbers = new int[numbers.Length / 4];
+            int[] lastNumbers = new int[numbers.Length / 4];
+            int[] middleNumbers = new int[numbers.Length / 2];
+            int[] sums = new int[numbers.Length / 2];
+
+            int i = 0;
+            int l = 0;
+            int k = 0;
+            while (i < numbers.Length / 4)
+            {
+                firstNumbers[i] = numbers[i];
+                i++;
+            }
+            while (i == numbers.Length / 4 || i < numbers.Length - numbers.Length/4)
+            {
+                middleNumbers[l] = numbers[i];
+                l++;
+                i++;
+            }
+            while (i == numbers.Length - numbers.Length /4 || i < numbers.Length)
+            {
+                lastNumbers[k] = numbers[i];
+                k++;
+                i++;
+            }
+
+            Array.Reverse(firstNumbers);
+            Array.Reverse(lastNumbers);
+            i = 0;
+            int m = 0;
+            int n = 0;
+
+            while (i < sums.Length)
+            {
+                if (i < sums.Length /2)
+                {
+                    sums[i] = firstNumbers[m] + middleNumbers[m];
+                    i++;
+                    m++;
+                }
+                else if (i > sums.Length /2 || i < sums.Length)
+                {
+                    sums[i] = lastNumbers[n] + middleNumbers[m];
+                    i++;
+                    n++;
+                    m++;
+                }
+                
+            }
+
+            Console.WriteLine(string.Join(" ", sums));
+        }
+    }
+}
