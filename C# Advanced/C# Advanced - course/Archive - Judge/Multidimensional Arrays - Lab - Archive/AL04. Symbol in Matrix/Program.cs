61ca9fd7e3be70d1f134c7dad86b3c983f1d26f7
Polinadrumeva <polina9230@gmail.com>
2022-04-08 22:56:40 +0300

    Add exercise from lab - archive

---
+++ C# Advanced/C# Advanced - course/Archive - Judge/Multidimensional Arrays - Lab - Archive/AL04. Symbol in Matrix/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AL04._Symbol_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int square = int.Parse(Console.ReadLine());
+            char[,] symbols = new char[square, square];
+
+            for (int i = 0; i < square; i++)
+            {
+                string input = Console.ReadLine();
+                for (int k = 0; k < square; k++)
+                {
+                    symbols[i, k] = input[k];
+                }
+            }
+
+            char searchingSymbol = char.Parse(Console.ReadLine());
+            for (int i = 0; i < square; i++)
+            {
+                for (int k = 0; k < square; k++)
+                {
+                    if (searchingSymbol == symbols[i, k])
+                    {
+                        Console.WriteLine($"({i}, {k})");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{searchingSymbol} does not occur in the matrix");
+        }
+    }
+}
