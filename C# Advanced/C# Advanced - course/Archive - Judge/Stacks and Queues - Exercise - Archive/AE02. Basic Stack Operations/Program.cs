d6472b2702c7cf660f67f20d61299fb40ada475d
Polinadrumeva <polina9230@gmail.com>
2022-04-09 12:31:41 +0300

    Add exercise from archive

---
+++ C# Advanced/C# Advanced - course/Archive - Judge/Stacks and Queues - Exercise - Archive/AE02. Basic Stack Operations/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AE02._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] commandNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Stack<int> numbers = new Stack<int>();
+            int count = 0;
+
+            int pushNumber = commandNumbers[0];
+            int popNumber = commandNumbers[1];
+            int peekNumber = commandNumbers[2];
+
+            int[] number = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < number.Length; i++)
+            {
+                numbers.Push(number[i]);
+                count++;
+                if (count == pushNumber)
+                {
+                    break;
+                }
+            }
+
+            for (int k = 0; k < popNumber; k++)
+            {
+                numbers.Pop();
+            }
+
+            if (numbers.Contains(peekNumber))
+            {
+                Console.WriteLine("true");
+            }
+            else if (numbers.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine($"{numbers.Min()}");
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - course/Archive - Judge/Stacks and Queues - Exercise - Archive/AE03. Maximum Element/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AE03._Maximum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfQueries = int.Parse(Console.ReadLine());
+            Stack<int> numbers = new Stack<int>();
+            for (int i = 0; i < numOfQueries; i++)
+            {
+                int[] cmdArgs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int commandType = cmdArgs[0];
+                
+                if (commandType == 1)
+                {
+                    int number = cmdArgs[1];
+                    numbers.Push(number);
+                }
+                else if (commandType == 2)
+                { 
+                    numbers.Pop();
+                }
+                else if (commandType == 3)
+                {
+                    Console.WriteLine(numbers.Max());
+                }
+                
+            }
+        }
+    }
+}
