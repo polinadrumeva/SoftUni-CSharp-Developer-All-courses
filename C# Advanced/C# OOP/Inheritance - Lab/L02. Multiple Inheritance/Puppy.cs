b7b1292a36a312dbc354a4349e7bca1e1978f024
Polina <polina9130@gmail.com>
2022-04-27 16:39:49 +0300

    Add inheritance - lab

---
+++ C# Advanced/C# OOP/Inheritance - Lab/L02. Multiple Inheritance/Puppy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L02._Multiple_Inheritance
+{
+    public class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+
+    }
+}
