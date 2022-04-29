a278e0b06c59b63dacee86702ed83bc16d6b6587
Polina <polina9130@gmail.com>
2022-04-29 16:11:10 +0300

    Add encapsulation - lab

---
+++ C# Advanced/C# OOP/Encapsulation - Lab/L01. Sort Persons by Name and Age/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L01._Sort_Persons_by_Name_and_Age
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var singleLine = Console.ReadLine()
+                    .Split(" ");
+                var person = new Person(singleLine[0], singleLine[1], int.Parse(singleLine[2]));
+                persons.Add(person);
+
+            }
+
+            var sortedPerson = persons
+                .OrderBy(x => x.FirstName)
+                .ThenBy(x => x.Age);
+
+            foreach (var person in sortedPerson)
+            {
+                Console.WriteLine(person.ToString());
+            }
+                
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/Encapsulation - Lab/L02. Salary Increase/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L01._Sort_Persons_by_Name_and_Age
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var singleLine = Console.ReadLine()
+                    .Split(" ");
+                var person = new Person(singleLine[0], singleLine[1], int.Parse(singleLine[2]), decimal.Parse(singleLine[3]));
+                persons.Add(person);
+
+            }
+
+            decimal percentage = decimal.Parse(Console.ReadLine());
+
+            persons.ForEach(p => p.IncreaseSalary(percentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+        
+        }
+
+    }
+}
