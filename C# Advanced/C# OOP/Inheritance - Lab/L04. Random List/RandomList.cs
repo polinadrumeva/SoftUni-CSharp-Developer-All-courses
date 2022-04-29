2c5a6a1daabf33cc13375e98c6157d651b698727
Polinadrumeva <polina9230@gmail.com>
2022-04-29 22:05:28 +0300

    Add inheritance - lab

---
+++ C# Advanced/C# OOP/Inheritance - Lab/L04. Random List/RandomList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L04._Random_List
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            random = new Random();
+        }
+
+        public string GetRandomElement()
+        { 
+            int index = random.Next(0, this.Count);
+            return this[index];
+        }
+
+        public void RemoveRandomElement()
+        { 
+           int index = random.Next(0, this.Count);
+            RemoveAt(index);
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/Inheritance - Lab/L05. Stack of Strings/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L05._Stack_of_Strings
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var stack = new StackOfStrings();
+
+            Console.WriteLine(stack.IsEmpty());
+
+            var list = new List<string> { "1", "2", "3" };
+            stack.AddRange(list);
+            Console.WriteLine(stack.IsEmpty());
+
+        }
+    }
+}
