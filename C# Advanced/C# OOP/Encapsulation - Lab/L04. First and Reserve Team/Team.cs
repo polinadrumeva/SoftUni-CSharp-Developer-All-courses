e316926a7ead30bffe1cc699e6d34fd9bd713b2a
Polinadrumeva <polina9230@gmail.com>
2022-05-02 22:03:04 +0300

    Add encapsulation lab

---
+++ C# Advanced/C# OOP/Encapsulation - Lab/L04. First and Reserve Team/Team.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L04._First_and_Reserve_Team
+{
+    public class Team 
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public string Name => this.name;
+        public IReadOnlyList<Person> FirstTeam => this.firstTeam;
+        public IReadOnlyList<Person> ReserveTeam => this.reserveTeam;
+        public Team (string name)
+        {
+            this.name = name;   
+            this.firstTeam = new List<Person> ();
+            this.reserveTeam = new List<Person> ();
+        }
+
+        public void AddPlayer( Person person)
+        {
+            if (person.Age < 40)
+            {
+                this.firstTeam.Add(person);
+            }
+            else
+            {
+                this.reserveTeam.Add(person);
+            }
+        }
+    
+    }
+}
