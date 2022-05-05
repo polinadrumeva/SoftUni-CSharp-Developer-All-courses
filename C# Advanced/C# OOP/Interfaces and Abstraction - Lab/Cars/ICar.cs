4938da1e732a2f670229ca9f357e457a33e5bc8b
Polinadrumeva <polina9230@gmail.com>
2022-05-05 22:43:54 +0300

    Add interface - lab

---
+++ C# Advanced/C# OOP/Interfaces and Abstraction - Lab/Cars/ICar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L02.Cars
+{
+    public interface ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        string Start();
+        string Stop();
+
+        void ToString();
+
+    }
+}
