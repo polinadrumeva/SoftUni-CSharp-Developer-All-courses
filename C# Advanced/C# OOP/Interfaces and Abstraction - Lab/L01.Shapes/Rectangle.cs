8127776ef0aea607186eb78faed42d29a732464b
Polinadrumeva <polina9230@gmail.com>
2022-05-05 13:26:12 +0300

    Add interface and abstraction - lab

---
+++ C# Advanced/C# OOP/Interfaces and Abstraction - Lab/L01.Shapes/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01.Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        public int Width { get; private set; }
+
+        public int Height { get; private set; }
+
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine(new string('*', this.Width));
+            
+                for (int j = 0; j < this.Height - 2; j++)
+                {
+                    Console.WriteLine("*" + new string(' ', this.Width - 2) + "*");
+                }
+
+            Console.WriteLine(new string('*', this.Width));
+        }
+
+    }
+}
