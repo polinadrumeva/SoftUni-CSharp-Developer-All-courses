330c6382b0f4872944d85f4935f601df4ddea3d6
Polina <polina9130@gmail.com>
2022-05-16 13:48:08 +0300

    Add stacks and queue - lab

---
+++ C# Advanced/C# Advanced - course/Stacks and Queues - Lab/L01. Reverse Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AL01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> reversedText = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                reversedText.Push(input[i]);
+            }
+
+            foreach (var item in reversedText)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
