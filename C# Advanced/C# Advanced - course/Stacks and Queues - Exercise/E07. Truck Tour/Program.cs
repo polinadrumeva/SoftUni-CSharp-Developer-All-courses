05dbef308306f8b9a06e3517177065ff2e836015
Polina <polina9130@gmail.com>
2022-05-17 17:26:19 +0300

    Add stacks and queues exercise

---
+++ C# Advanced/C# Advanced - course/Stacks and Queues - Exercise/E07. Truck Tour/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E07._Truck_Tour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> pumps = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int[] petrolPump = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                pumps.Push(petrolPump[0]);
+                pumps.Push(petrolPump[1]);
+            }
+
+            Console.WriteLine(pumps.Min());
+        }
+    }
+}
