ee3ff822f9144637cc0a7787454b29e7d9ba988b
Polina <polina9130@gmail.com>
2022-05-19 12:28:45 +0300

    Add balanced

--- C# Advanced/C# Advanced - course/Stacks and Queues - Exercise/E08.Balanced Parenthesis/Program.cs
+++ C# Advanced/C# Advanced - course/Stacks and Queues - Exercise/E08.Balanced Parenthesis/Program.cs
@@ -8,19 +8,52 @@ namespace E08._Balanced_Parenthesis
         static void Main(string[] args)
         {
             string sequence = Console.ReadLine();
-            Stack<char> stack = new Stack<char>(sequence);
+            Stack<char> stack = new Stack<char>();
+            bool areBalance = false;
 
             for (int i = 0; i < sequence.Length; i++)
             {
-                if (sequence[i] == '(')
+                if (sequence[i] == '(' || sequence[i] == '{' || sequence[i] == '[')
                 {
                     stack.Push(sequence[i]);
                 }
-                else if (sequence[i] == ')')
+                else if (sequence[i] == ')' || sequence[i] == '}' || sequence[i] == ']')
                 {
-                    int startIndex = stack.Pop();
-                    Console.WriteLine(sequence.Substring(startIndex, i - startIndex + 1));
+                    if (stack.Count == 0)
+                    {
+                        areBalance = false;
+                        break;
+                    }
+
+                    char lastElement = stack.Pop();
+                    if (lastElement == '(' && sequence[i] == ')')
+                    { 
+                        areBalance = true;
+                    }
+                    else if (lastElement == '{' && sequence[i] == '}')
+                    {
+                        areBalance = true;
+                    }
+                    else if (lastElement == '[' && sequence[i] == ']')
+                    {
+                        areBalance = true;
+                    }
+                    else
+                    {
+                        areBalance = false;
+                        break;
+                    }
                 }
+
+            }
+
+            if (areBalance)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
             }
         }
     }
