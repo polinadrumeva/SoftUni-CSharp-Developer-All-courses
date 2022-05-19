5516dfa60352f0d37bb2ce02594219463a286874
Polina <polina9130@gmail.com>
2022-05-19 11:14:34 +0300

    Add

---
+++ C# Advanced/C# Advanced - course/Multidimensional Arrays - Exercise/E01. Diagonal Difference/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace AE02._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int square = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[square, square];
+
+            for (int i = 0; i < square; i++)
+            {
+                int[] numbers = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+
+                for (int j = 0; j < square; j++)
+                {
+                    matrix[i, j] = numbers[j];
+                }
+            }
+
+            long primarySum = 0;
+            long secondarySum = 0;
+
+            for (int i = 0; i < square; i++)
+            {
+                primarySum += matrix[i, i];
+            }
+
+            int count = 0;
+            for (int j = square - 1; j >= 0; j--)
+            {
+                secondarySum += matrix[count, j];
+                count++;
+            }
+
+            long difference = Math.Abs(primarySum - secondarySum);
+            Console.WriteLine(difference);
+
+        }
+    }
+}
