3e4ee28d45913778515f7603e4aae42eea687111
Polinadrumeva <polina9230@gmail.com>
2022-05-23 23:05:33 +0300

    Add largest 3 numbers

--- C# Advanced/C# Advanced - course/Sets and Dictionaries Advanced - Lab/L03. Largest 3 Numbers/Program.cs
+++ C# Advanced/C# Advanced - course/Sets and Dictionaries Advanced - Lab/L03. Largest 3 Numbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace L03._Largest_3_Numbers
 {
@@ -6,7 +7,25 @@ namespace L03._Largest_3_Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).OrderByDescending(x => x).ToArray();
+            for (int i = 0; i < 3; i++)
+            {
+                if (numbers.Length < 3)
+                {
+                    for (int j = 0; j < numbers.Length; j++)
+                    {
+                        Console.Write($"{numbers[j]} ");
+                    }
+                    Console.WriteLine();
+                    break;
+                }
+                else
+                {
+                    Console.Write($"{numbers[i]} ");
+                }
+                
+            }
+            Console.WriteLine();
         }
     }
 }
