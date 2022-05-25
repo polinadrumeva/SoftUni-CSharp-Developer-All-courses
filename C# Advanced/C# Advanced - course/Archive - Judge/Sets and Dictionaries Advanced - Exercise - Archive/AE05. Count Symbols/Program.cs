36c92c70dcd3eb46e67adef8da7955e99c40c03c
Polina <polina9130@gmail.com>
2022-05-25 12:01:05 +0300

    Add exercise for archive

---
+++ C# Advanced/C# Advanced - course/Archive - Judge/Sets and Dictionaries Advanced - Exercise - Archive/AE05. Count Symbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AE05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Dictionary<char, int> symbols = new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!symbols.ContainsKey(text[i]))
+                {
+                    symbols.Add(text[i], 0);
+                }
+
+                symbols[text[i]]++;
+            }
+
+            foreach (var item in symbols.OrderBy( x => x.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
