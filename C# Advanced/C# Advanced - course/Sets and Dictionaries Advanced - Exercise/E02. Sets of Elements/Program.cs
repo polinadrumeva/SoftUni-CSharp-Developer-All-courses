d55ee8e16304f761e1a00f6e9533bd6369519f25
Polina <polina9130@gmail.com>
2022-05-25 09:58:40 +0300

    Add exercise

---
+++ C# Advanced/C# Advanced - course/Sets and Dictionaries Advanced - Exercise/E02. Sets of Elements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E02._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lengthNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            HashSet<double> numbers = new HashSet<double>();
+            HashSet<double> dublicateNumbers = new HashSet<double>();
+
+            int first = lengthNumbers[0];
+            int second = lengthNumbers[1];
+
+            for (int i = 0; i < first; i++)
+            {
+                double num = double.Parse(Console.ReadLine());
+                numbers.Add(num);
+            }
+
+            for (int j = 0; j < second; j++)
+            {
+                double number = double.Parse(Console.ReadLine());
+                if (numbers.Contains(number))
+                {
+                    dublicateNumbers.Add(number);
+                }
+            }
+
+
+            foreach (var item in dublicateNumbers)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - course/Sets and Dictionaries Advanced - Exercise/E08. Ranking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E08._Ranking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
