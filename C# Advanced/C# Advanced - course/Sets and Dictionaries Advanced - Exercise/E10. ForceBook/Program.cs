cef6b517b7c7cb49fc3d7337b4a673a73848967e
Polina <polina9130@gmail.com>
2022-05-26 12:10:20 +0300

    Add

--- C# Advanced/C# Advanced - course/Sets and Dictionaries Advanced - Exercise/E10. ForceBook/Program.cs
+++ C# Advanced/C# Advanced - course/Sets and Dictionaries Advanced - Exercise/E10. ForceBook/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace E01._Unique_Usernames
 {
@@ -7,21 +8,65 @@ namespace E01._Unique_Usernames
     {
         static void Main(string[] args)
         {
-            int number = int.Parse(Console.ReadLine());
-            HashSet<string> names = new HashSet<string>();
+            Dictionary<string, HashSet<string>> forceSides = new Dictionary<string, HashSet<string>>();
+            List<string> sides = new List<string>();
 
-            for (int i = 0; i < number; i++)
+            string command;
+            while ((command = Console.ReadLine()) != "Lumpawaroo")
             {
-                string name = Console.ReadLine();
-                if (!names.Contains(name))
+                if (command.Contains('|'))
                 {
-                    names.Add(name);
+                    string[] cmdArg = command.Split(" | ");
+                    string forseSide = cmdArg[0];
+                    if (!forceSides.ContainsKey(forseSide))
+                    {
+                        forceSides.Add(forseSide, new HashSet<string>());
+                        sides.Add(forseSide);
+                    }
+
+                    string forseUser = cmdArg[1];
+                    if (!forceSides[forseSide].Contains(forseUser))
+                    {
+                        forceSides[forseSide].Add(forseUser);
+                    }
                 }
+                else if (command.Contains('>'))
+                {
+                    string[] cmdArg = command.Split(" -> ");
+                    string forseUserChange = cmdArg[0];
+                    string forseSideChange = cmdArg[1];
+
+                    if (sides[0] != forseSideChange && forceSides.ContainsKey(sides[0]))
+                    {
+                        forceSides[sides[0]].Remove(forseUserChange);
+                        forceSides[forseSideChange].Add(forseUserChange);
+                        Console.WriteLine($"{forseUserChange} joins the {forseSideChange} side!");
+                    }
+                    else if (sides[1] != forseSideChange && forceSides.ContainsKey(sides[1]))
+                    {
+                        forceSides[sides[1]].Remove(forseUserChange);
+                        forceSides[forseSideChange].Add(forseUserChange);
+                        Console.WriteLine($"{forseUserChange} joins the {forseSideChange} side!");
+                    }
+                    else if (!forceSides.ContainsKey(sides[0]) || !forceSides.ContainsKey(sides[1]))
+                    {
+                        forceSides[forseSideChange].Add(forseUserChange);
+                        Console.WriteLine($"{forseUserChange} joins the {forseSideChange} side!");
+                    }
+                }
+                   
             }
 
-            foreach (var item in names)
+            foreach (var side in forceSides.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
             {
-                Console.WriteLine(item);
+                if (!(side.Value.Count == 0))
+                {
+                    Console.WriteLine($"Side: {side.Key}, Members: {side.Value.Count}");
+                    foreach (var member in side.Value.OrderBy(x => x))
+                    {
+                        Console.WriteLine($"! {member}");
+                    }
+                }
             }
         }
     }
