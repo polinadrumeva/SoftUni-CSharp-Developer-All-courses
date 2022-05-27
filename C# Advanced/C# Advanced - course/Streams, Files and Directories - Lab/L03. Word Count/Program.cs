b1e78cb1278b146f50dbfc93e17be8ce6a76765b
Polina <polina9130@gmail.com>
2022-05-27 15:15:42 +0300

    Add lab-exercise

---
+++ C# Advanced/C# Advanced - course/Streams, Files and Directories - Lab/L03. Word Count/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L03._Word_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - course/Streams, Files and Directories - Lab/L07. Folder Size/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L07._Folder_Size
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
