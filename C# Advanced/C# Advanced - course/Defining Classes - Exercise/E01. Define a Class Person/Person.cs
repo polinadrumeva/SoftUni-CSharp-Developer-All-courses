7909461d4e5132de529fdc42a0bac6abe72cb456
Polina <polina9130@gmail.com>
2022-06-02 17:25:27 +0300

    add first

---
+++ C# Advanced/C# Advanced - course/Defining Classes - Exercise/E01. Define a Class Person/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get ;  set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+
+        }
+    }
+}
