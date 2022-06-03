708802bc33741541539e1ba3ec5a097d0bd360ae
Polina <polina9130@gmail.com>
2022-06-03 16:47:36 +0300

    add

---
+++ C# Advanced/C# Advanced - course/Defining Classes - Exercise/E03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();   
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string command = Console.ReadLine();
+                string name = command.Split(" ")[0];
+                int age = int.Parse(command.Split(" ")[1]);
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+            Person oldestPerson = family.GetOldestMember();
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+}
