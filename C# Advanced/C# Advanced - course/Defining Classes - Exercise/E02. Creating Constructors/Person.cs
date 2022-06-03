78b5fe964ed5ac1d84c43702a1e91657e2389176
Polina <polina9130@gmail.com>
2022-06-03 09:35:19 +0300

    add defining classes

--- C# Advanced/C# Advanced - course/Defining Classes - Exercise/E02. Creating Constructors/Person.cs
+++ C# Advanced/C# Advanced - course/Defining Classes - Exercise/E02. Creating Constructors/Person.cs
@@ -6,7 +6,6 @@ namespace DefiningClasses
 {
     public class Person
     {
-
         private string name;
         private int age;
 
@@ -25,7 +24,18 @@ namespace DefiningClasses
 
         public Person()
         {
-
+            this.name = "No name";
+            this.age = 1;
+        }
+        public Person(int age)
+        {
+            this.name = "No name";
+            this.age = age;
+        }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
         }
     }
 }
