4ba72ea3d27868d3dc21f79fc1b2d8062a45b5e8
Polinadrumeva <polina9230@gmail.com>
2022-06-05 22:18:27 +0300

    add raw data - exercise

---
+++ C# Advanced/C# Advanced - course/Defining Classes - Exercise/E07. Raw Data/Engine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E07.RawData
+{
+    public class Engine
+    {
+        public double Speed { get; set; }
+
+        public double Power { get; set; }
+
+        public Engine(double speed, double Power)
+        {
+            this.Power = Power;
+            this.Speed = speed;
+        }
+    }
+}
