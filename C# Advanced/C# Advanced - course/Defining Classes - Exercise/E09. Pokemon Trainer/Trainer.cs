5bdedf75f419045c8043af065a63407aa0410e6c
Polina <polina9130@gmail.com>
2022-06-06 16:24:52 +0300

    add Pokemon trainer

---
+++ C# Advanced/C# Advanced - course/Defining Classes - Exercise/E09. Pokemon Trainer/Trainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E09.PokemonTrainer
+{
+    public class Trainer
+    {
+        public string Name { get; set; }
+        public int NumberOfBadges { get; set; }
+        public List<Pokemon> CollectionOfPokemons { get; set; }
+
+        public Trainer(string trainerName, int numberOfBadges, List<Pokemon> pokemons)
+        {
+            this.Name = trainerName;
+            this.NumberOfBadges = numberOfBadges;
+            this.CollectionOfPokemons = pokemons;
+        }
+    }
+}
