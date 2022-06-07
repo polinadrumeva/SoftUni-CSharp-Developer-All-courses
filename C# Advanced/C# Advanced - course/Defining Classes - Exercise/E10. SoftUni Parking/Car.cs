a55c98f34ac4297bbd452e14d9ec4f730187d311
Polina <polina9130@gmail.com>
2022-06-07 15:39:27 +0300

    Add parking system

---
+++ C# Advanced/C# Advanced - course/Defining Classes - Exercise/E10. SoftUni Parking/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+        public string RegistrationNumber { get; set; }
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.RegistrationNumber = registrationNumber;
+        }
+        public override string ToString() 
+        {
+            string toString = $"Make: {Make}\nModel: {Model}\nHorsePower: {HorsePower}\nRegistrationNumber: {RegistrationNumber}";
+            return toString;
+        }
+       
+    }
+}
