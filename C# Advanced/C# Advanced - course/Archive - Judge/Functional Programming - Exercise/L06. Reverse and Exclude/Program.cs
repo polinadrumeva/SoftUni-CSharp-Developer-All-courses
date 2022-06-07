28d089c092525d18b04b8f3761a6bf9e19c855a5
Polina <polina9130@gmail.com>
2022-06-07 16:27:34 +0300

    Add archive exercises

---
+++ C# Advanced/C# Advanced - course/Archive - Judge/Functional Programming - Exercise/L06. Reverse and Exclude/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace AE06._Reverse_and_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int divisibleNumber = int.Parse(Console.ReadLine());
+
+            numbers.Reverse();
+            Predicate<int> isDivisible = x => x % divisibleNumber == 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (isDivisible(numbers[i]))
+                {
+                    numbers.Remove(numbers[i]);
+                    i = -1;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+
+
+        }
+    }
+}
