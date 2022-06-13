78190cbe85f37df28c6ed3d05475b30c731301f6
Polinadrumeva <polina9230@gmail.com>
2022-06-14 00:18:59 +0300

    add generics

---
+++ C# Advanced/C# Advanced - course/Generics - Exercise/E07. Tuple/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace E07.Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var personInfo = Console.ReadLine().Split();
+            var fullName = $"{personInfo[0]} {personInfo[1]}";
+            var adress = $"{personInfo[2]}";
+
+            var nameAndBeer = Console.ReadLine().Split();
+            var name = nameAndBeer[0];
+            var litersBeer = int.Parse(nameAndBeer[1]);
+
+            var numbersInput = Console.ReadLine().Split();  
+            var intNum = int.Parse(numbersInput[0]);
+            var doubleNum = double.Parse(numbersInput[1]);
+
+            Tuple<string, string> firstTuple = new Tuple<string, string>(fullName, adress);
+            Tuple<string, int> secondTuple = new Tuple<string, int>(name, litersBeer);
+            Tuple<int, double> thirdTuple = new Tuple<int, double>(intNum, doubleNum);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - course/Generics - Exercise/E08. Threeuple/Threeuple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E08.Threeuple
+{
+    public class Threeuple<TFirst, TSecond, TThird>
+    {
+        public TFirst FirstElement { get; private set; }
+        public TSecond SecondElement { get; private set; }
+        public TThird ThirdElement { get; private set; }
+
+
+        public Threeuple(TFirst firstelement, TSecond secondelement, TThird thirdelement)
+        {
+            this.FirstElement = firstelement;
+            this.SecondElement = secondelement;
+            this.ThirdElement = thirdelement;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstElement} -> {SecondElement} -> {ThirdElement}";
+        }
+    }
+}
