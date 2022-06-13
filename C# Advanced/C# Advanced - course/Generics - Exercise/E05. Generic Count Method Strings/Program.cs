08b8564cc572ef5eee160f15c1e11b59dbb0b170
Polinadrumeva <polina9230@gmail.com>
2022-06-13 22:17:30 +0300

    add generics - exercise

---
+++ C# Advanced/C# Advanced - course/Generics - Exercise/E05. Generic Count Method Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E05._Generic_Count_Method_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - course/Generics - Exercise/E09. Custom Linked List/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E09._Custom_Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
