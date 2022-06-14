147b0135c87b32ef472be16590e8b501cd4f3523
Polina <polina9130@gmail.com>
2022-06-14 10:15:45 +0300

    add exam exercise

---
+++ C# Advanced/C# Advanced - course/Exams  - Judge/Exam - 26.06.2021/Ex02.Survivor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex02.Survivor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - course/Exams  - Judge/Exam - 26.06.2021/Ex03. Ski Rental/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex03._Ski_Rental
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
