b33c8ba0ccff04b709f40e6abf86e351fe1235e1
Polinadrumeva <polina9230@gmail.com>
2022-06-19 23:35:21 +0300

    add

--- Algorithms Fundamentals/Recursion and Backtracking - Lab/L03. Generating Vectors/Program.cs
+++ Algorithms Fundamentals/Recursion and Backtracking - Lab/L03. Generating Vectors/Program.cs
@@ -6,7 +6,24 @@ namespace L03._Generating_Vectors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int[] vector = new int[n];
+            GetVector(vector, 0);
+        }
+
+        private static void GetVector(int[] vector, int index)
+        {
+            if (index >= vector.Length)
+            {
+                Console.WriteLine(string.Join("", vector));
+                return;
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                vector[index] = i;
+                GetVector(vector, index + 1);
+            }
         }
     }
 }
