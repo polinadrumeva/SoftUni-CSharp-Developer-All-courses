190816fca530eb43d20235c26438bb747719bca4
Polina <polina9130@gmail.com>
2022-06-21 13:03:15 +0300

    add new exam exercise

---
+++ C# Advanced/C# Advanced - course/Exams  - Judge/Adv.Exam - 20.02.2021/01. The Fight for Gondor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._The_Fight_for_Gondor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
