a0d7a831b666ae556c7ac03140190502fbeb3a78
Polinadrumeva <polina9230@gmail.com>
2022-06-22 22:19:42 +0300

    add

---
+++ C# Advanced/C# Advanced - course/Exams  - Judge/Adv.Exam - 25.10.2020/03. Classroom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Classroom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
