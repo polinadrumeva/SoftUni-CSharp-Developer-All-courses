400385c5d1f8ec26c9dc9ef93991c5bf24fbe8ea
Polinadrumeva <polina9230@gmail.com>
2022-06-29 22:48:54 +0300

    add searching algorithms

---
+++ Algorithms Fundamentals/Searching, Sorting and Greedy Algorithms - Lab/06. Merge Sort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06._Merge_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Algorithms Fundamentals/Searching, Sorting and Greedy Algorithms - Lab/08. Set Cover/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08._Set_Cover
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
