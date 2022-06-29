7b0823e426ed64225ebd0e89d386440a43b1ca26
Polina <polina9130@gmail.com>
2022-06-29 14:30:49 +0300

    add n choose k count

--- Algorithms Fundamentals/Combinatorial Problems - Lab/07. N Choose K Count/Program.cs
+++ Algorithms Fundamentals/Combinatorial Problems - Lab/07. N Choose K Count/Program.cs
@@ -6,7 +6,20 @@ namespace _07._N_Choose_K_Count
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetBinom(n,k));
+        }
+
+        private static int GetBinom(int row, int col)
+        {
+            if (row <= 1 || col == 0 || row == col)
+            {
+                return 1;
+            }
+
+            return GetBinom(row -1, col) + GetBinom(row - 1, col - 1);
         }
     }
 }
