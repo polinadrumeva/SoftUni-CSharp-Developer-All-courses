3d4ef86e67f537b532bccaefc6786d1b028d1b6a
Polina <polina9130@gmail.com>
2022-06-30 14:14:47 +0300

    add algorithms problem

--- Algorithms Fundamentals/Searching, Sorting and Greedy Algorithms - Lab/05. Quicksort/Program.cs
+++ Algorithms Fundamentals/Searching, Sorting and Greedy Algorithms - Lab/05. Quicksort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _05._Quicksort
 {
@@ -6,7 +7,52 @@ namespace _05._Quicksort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            QuickSort(numbers, 0, numbers.Length - 1);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void QuickSort(int[] numbers, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            var pivot = start;
+            var left = start + 1;
+            var right = end;
+
+            while (left <= right)
+            {
+                if (numbers[left] > numbers[pivot] && numbers[right] < numbers[pivot])
+                {
+                    Swap(numbers, left, right);
+                }
+                if (numbers[left] <= numbers[pivot])
+                {
+                    left++;
+                }
+                if (numbers[right] >= numbers[pivot])
+                {
+                    right--;
+                }
+
+            }
+
+            Swap(numbers, pivot, right);
+            QuickSort(numbers, start, right - 1);
+            QuickSort(numbers, right + 1, end);
+
+        }
+
+        private static void Swap(int[] numbers, int first, int second)
+        {
+            int current = numbers[first];
+            numbers[first] = numbers[second];
+            numbers[second] = current;
         }
     }
 }
