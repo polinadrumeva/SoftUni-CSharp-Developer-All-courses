0fcc033865fa683556e86a85d8d3696dc466bcb4
Polina <polina9130@gmail.com>
2022-06-30 16:36:16 +0300

    add algorithms basic exercise

---
+++ C# Advanced/C# Advanced - course/Basic Algorithms/02. Recursive Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02._Recursive_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(CalculateFactorial(n));
+        }
+
+        private static int CalculateFactorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * CalculateFactorial(n - 1);
+        }
+    }
+}
