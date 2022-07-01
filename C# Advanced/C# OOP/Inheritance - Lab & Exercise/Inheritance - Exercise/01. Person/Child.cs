d91f7c99953d2e910e2bf39ef60ca0b1369cc484
Polina <polina9130@gmail.com>
2022-07-01 17:30:01 +0300

    add person class

---
+++ C# Advanced/C# OOP/Inheritance - Lab & Exercise/Inheritance - Exercise/01. Person/Child.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Child
+    {
+    }
+}
---
+++ C# Advanced/C# OOP/Inheritance - Lab & Exercise/Inheritance - Exercise/01. Person/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get; set; }
+        public int Age 
+        { 
+            get 
+            {
+                return this.Age; 
+            } 
+            set
+            {
+                if (value >= 0)
+                {
+                    this.Age = value;
+                }
+                
+            } 
+        }
+    }
+}
