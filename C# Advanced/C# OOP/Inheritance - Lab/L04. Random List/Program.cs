bb5836280cb69b1394c5f16ed7d03dbeca5b3bea
Polina <polina9130@gmail.com>
2022-07-01 09:24:13 +0300

    add

---
+++ C# Advanced/C# OOP/Inheritance - Lab/L04. Random List/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L04._Random_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/Inheritance - Lab/L05. Stack of Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L05._Stack_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
