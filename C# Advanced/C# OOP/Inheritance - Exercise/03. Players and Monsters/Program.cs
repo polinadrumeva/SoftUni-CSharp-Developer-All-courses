8eb8964b0e86c387fd8dd0a1bc05d43417eabca3
Polina <polina9130@gmail.com>
2022-07-01 15:09:55 +0300

    add inheritance exercise

---
+++ C# Advanced/C# OOP/Inheritance - Exercise/03. Players and Monsters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Players_and_Monsters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/Inheritance - Exercise/05. Restaurant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05._Restaurant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
