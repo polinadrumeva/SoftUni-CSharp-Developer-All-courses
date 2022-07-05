2ec98b77a8442fcc59d957de12d9116b096becef
Polinadrumeva <polina9230@gmail.com>
2022-07-05 23:39:39 +0300

    addd

---
+++ C# Advanced/C# OOP/Encapsulation - Lab & Exercise/Encapsulation - Exercise/3. Shopping Spree/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Product
+    {
+        private string name;
+        private decimal cost;
+
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+
+        public Product(string name, decimal price)
+        {
+            this.Name = name;   
+            this.Cost = price;
+        }
+    }
+}
