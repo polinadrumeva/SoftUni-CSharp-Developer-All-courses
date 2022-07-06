173e1c3a80dc3db7074462f79b00c7455f4c8136
Polina <polina9130@gmail.com>
2022-07-06 16:26:33 +0300

    add pizza

---
+++ C# Advanced/C# OOP/Encapsulation - Lab & Exercise/Encapsulation - Exercise/4. Pizza Calories/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PizzaCalories
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                string[] pizzaData = Console.ReadLine().Split(" ");
+                string[] doughData = Console.ReadLine().Split(" ");
+                Dough dough = new Dough(doughData[1], doughData[2], double.Parse(doughData[3]));
+                Pizza pizza = new Pizza(pizzaData[1], dough);
+                try
+                {
+                    string toppingData;
+                    while ((toppingData = Console.ReadLine()) != "END")
+                    {
+                        string[] dataTopping = toppingData.Split(" ");
+                        ToppingClass topping = new ToppingClass(dataTopping[1], double.Parse(dataTopping[2]));
+                        pizza.Add(topping);
+
+                    }
+
+                    Console.WriteLine($"{pizza.Name} - {pizza.CalculateCalories():f2} Calories.");
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/Encapsulation - Lab & Exercise/Encapsulation - Exercise/4. Pizza Calories/ToppingClass.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class ToppingClass
+    {
+        private string toppingType;
+        private double grams;
+        private double toppingModifier;
+
+        public string ToppingType
+        {
+            get
+            {
+                return this.toppingType;
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value) || (value.ToLower() != "meat" && value.ToLower() != "veggies" && value.ToLower() != "cheese" && value.ToLower() != "sauce"))
+                {
+                    throw new Exception($"Cannot place {value} on top of your pizza.");
+                }
+
+                this.toppingType = value;
+            }
+        }
+        
+        public double Grams
+        {
+            get
+            {
+                return this.grams;
+            }
+            set
+            {
+                if (value < 1 || value > 50)
+                {
+                    throw new Exception($"{this.toppingType} weight should be in the range[1..50].");
+                }
+
+                this.grams = value;
+            }
+        }
+
+        public double ToppingModifier
+        {
+            get
+            {
+                return this.toppingModifier;
+            }
+            set
+            {
+                if (this.toppingType.ToLower() == "meat")
+                {
+                    this.toppingModifier = 1.2;
+                }
+                else if (this.toppingType.ToLower() == "veggies")
+                {
+                    this.toppingModifier = 0.8;
+                }
+                else if (this.toppingType.ToLower() == "cheese")
+                {
+                    this.toppingModifier = 1.1;
+                }
+                else if (this.toppingType.ToLower() == "sauce")
+                {
+                    this.toppingModifier = 0.9;
+                }
+            }
+        }
+
+        public ToppingClass(string toppingType, double grams)
+        {
+            this.ToppingType = toppingType;
+            this.Grams = grams;
+            this.ToppingModifier = toppingModifier;
+         
+        }
+
+        public double CalculateCalories()
+         =>  (this.Grams * this.ToppingModifier) * 2;
+
+    }
+}
