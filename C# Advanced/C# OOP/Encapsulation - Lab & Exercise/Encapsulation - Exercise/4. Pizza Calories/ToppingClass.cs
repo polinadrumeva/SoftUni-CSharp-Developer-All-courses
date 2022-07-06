c645cb19106e194e8f8cdc341016375dbc163783
Polina <polina9130@gmail.com>
2022-07-06 16:41:45 +0300

    add changes in range

--- C# Advanced/C# OOP/Encapsulation - Lab & Exercise/Encapsulation - Exercise/4. Pizza Calories/ToppingClass.cs
+++ C# Advanced/C# OOP/Encapsulation - Lab & Exercise/Encapsulation - Exercise/4. Pizza Calories/ToppingClass.cs
@@ -37,7 +37,7 @@ namespace PizzaCalories
             {
                 if (value < 1 || value > 50)
                 {
-                    throw new Exception($"{this.toppingType} weight should be in the range[1..50].");
+                    throw new Exception($"{this.toppingType} weight should be in the range [1..50].");
                 }
 
                 this.grams = value;
