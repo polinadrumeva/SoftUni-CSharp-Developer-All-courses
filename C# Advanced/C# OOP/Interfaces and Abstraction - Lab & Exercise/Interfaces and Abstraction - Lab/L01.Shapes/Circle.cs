31dbf210035d2d487b4b748d1f4ee89da3e644c7
Polina <polina9130@gmail.com>
2022-07-07 10:17:42 +0300

    add shapes

---
+++ C# Advanced/C# OOP/Interfaces and Abstraction - Lab & Exercise/Interfaces and Abstraction - Lab/L01.Shapes/Circle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+        public int Radius
+        {
+            get
+            {
+                return this.radius;
+            }
+            private set
+            {
+                this.radius = value;
+            }
+        }
+
+        public Circle(int radius)
+        {
+            this.Radius = radius;
+        }
+
+        public void Draw()
+        {
+            double rin = this.radius - 0.4;
+            double rout = this.radius + 0.4;
+
+            
+            for (double i = this.radius; i >= -this.radius; --i)
+            {
+                for (double j = -this.radius; j < rout; j += 0.5)
+                {
+                    var distance = (i * i) + (j * j);
+                    if (distance >= rin * rin && distance <= rout * rout)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
