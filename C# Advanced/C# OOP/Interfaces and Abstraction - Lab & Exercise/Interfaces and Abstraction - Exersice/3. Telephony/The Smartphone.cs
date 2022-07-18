c48fe682332204968a49198ee2f28c74b8a62b2e
Polina <polina9130@gmail.com>
2022-07-18 17:13:16 +0300

    адд

---
+++ C# Advanced/C# OOP/Interfaces and Abstraction - Lab & Exercise/Interfaces and Abstraction - Exersice/3. Telephony/The Smartphone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3.Telephony
+{
+    public class The_Smartphone : ISmartphone
+    {
+        public void Call()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SearchWeb()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/Interfaces and Abstraction - Lab & Exercise/Interfaces and Abstraction - Exersice/3. Telephony/The StationaryPhone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3.Telephony
+{
+    public class The_StationaryPhone : IStationarPhone
+    {
+        public void Call(s)
+        {
+            if (true)
+            {
+
+            }
+        }
+    }
+}
