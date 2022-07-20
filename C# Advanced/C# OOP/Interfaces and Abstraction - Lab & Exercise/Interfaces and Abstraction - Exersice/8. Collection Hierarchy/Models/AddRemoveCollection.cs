55309dd2f44d06a5705b3e18115b5c8991c4c7f2
Polina <polina9130@gmail.com>
2022-07-20 16:09:49 +0300

    add collection hierarchy

---
+++ C# Advanced/C# OOP/Interfaces and Abstraction - Lab & Exercise/Interfaces and Abstraction - Exersice/8. Collection Hierarchy/Models/AddRemoveCollection.cs
@@ -0,0 +1,34 @@
+﻿namespace CollectionHierarchy.Models
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Interfaces;
+
+    public class AddRemoveCollection<T> : IAddRemoveCollection<T>
+
+    {
+        private readonly IList<T> data;
+
+        public AddRemoveCollection()
+        {
+            this.data = new List<T>();
+        }
+
+        public int Add(T item)
+        {
+            this.data.Insert(0, item);
+            return 0;
+        }
+
+        public T Remove()
+        {
+            T toRemove = this.data.LastOrDefault();
+            if (toRemove != null)
+            { 
+                this.data.Remove(toRemove);
+            }
+
+            return toRemove;
+        }
+    }
+}
