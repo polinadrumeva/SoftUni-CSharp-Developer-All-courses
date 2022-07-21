a8c6fb335533b910910ce37c425f22bcd908f783
Polina <polina9130@gmail.com>
2022-07-21 16:10:22 +0300

    add money transactions

--- C# Advanced/C# OOP/Exception Handling - Exercise/E04.Sum of Integers/Program.cs
+++ C# Advanced/C# OOP/Exception Handling - Exercise/E04.Sum of Integers/Program.cs
@@ -6,7 +6,31 @@ namespace E04.Sum_of_Integers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] all = Console.ReadLine().Split(" ");
+            int sum = 0;
+
+            foreach (var item in all)
+            {
+                try
+                {
+                    int number = int.Parse(item);
+                    sum += number;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{item}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{item}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{item}' processed - current sum: {sum}");
+                }
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
         }
     }
 }
--- C# Advanced/C# OOP/Exception Handling - Exercise/E06.Money Transactions/Program.cs
+++ C# Advanced/C# OOP/Exception Handling - Exercise/E06.Money Transactions/Program.cs
@@ -1,12 +1,86 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace E06.Money_Transactions
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int,BankAcount> allaccount = new Dictionary<int, BankAcount>(); 
+
+            string[] all = Console.ReadLine().Split(",");
+            for (int i = 0; i < all.Length; i++)
+            {
+                string[] current = all[i].Split("-");
+                BankAcount newaccount = new BankAcount(int.Parse(current[0]), double.Parse(current[1]));
+                allaccount.Add(int.Parse(current[0]),newaccount);
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] cmdArg = command.Split(" ");
+
+                try
+                {
+                    string typeCmd = cmdArg[0];
+                    if (typeCmd == "Deposit")
+                    {
+                        int account = int.Parse(cmdArg[1]);
+                        if (!allaccount.ContainsKey(account))
+                        {
+                            throw new InvalidOperationException("Invalid account!");
+                        }
+                        double balance = double.Parse(cmdArg[2]);
+                        allaccount[account].Balance += balance;
+                        Console.WriteLine($"Account {account} has new balance: {allaccount[account].Balance:f2}");
+                    }
+                    else if (typeCmd == "Withdraw")
+                    {
+                        int account = int.Parse(cmdArg[1]);
+                        if (!allaccount.ContainsKey(account))
+                        {
+                            throw new InvalidOperationException("Invalid account!");
+                        }
+                        double balance = double.Parse(cmdArg[2]);
+                        if (balance > allaccount[account].Balance)
+                        {
+                            throw new IndexOutOfRangeException("Insufficient balance!");
+                        }
+                        else
+                        {
+                            allaccount[account].Balance -= balance; 
+                            Console.WriteLine($"Account {account} has new balance: {allaccount[account].Balance:f2}");
+                        }
+
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Invalid command!");
+                    }
+
+
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (InvalidOperationException invex)
+                {
+                    Console.WriteLine(invex.Message);
+                }
+                catch (IndexOutOfRangeException outEx)
+                {
+                    Console.WriteLine(outEx.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Enter another command");
+                }
+                
+
+            }
         }
     }
 }
