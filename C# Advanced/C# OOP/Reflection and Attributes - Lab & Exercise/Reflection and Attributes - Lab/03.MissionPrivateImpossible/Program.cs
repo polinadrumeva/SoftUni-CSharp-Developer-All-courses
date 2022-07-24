0d65772bd955b6098e540b350064f5dc88ef6196
Polinadrumeva <polina9230@gmail.com>
2022-07-24 23:08:14 +0300

    add template to reflection lab

---
+++ C# Advanced/C# OOP/Reflection and Attributes - Lab & Exercise/Reflection and Attributes - Lab/03.MissionPrivateImpossible/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.MissionPrivateImpossible
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
