6c74a15afcb35160b8b67b1cef40f25afc2a2954
Polina <polina9130@gmail.com>
2022-07-26 15:31:27 +0300

    add skeleton unit testing exercise

---
+++ C# Advanced/C# OOP/Unit Testing - Exersice/CarManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
