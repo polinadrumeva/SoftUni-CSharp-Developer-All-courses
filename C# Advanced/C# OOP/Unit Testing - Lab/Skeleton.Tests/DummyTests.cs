1cc1ef0a73627791d2b7338598cbb2a519990b2d
Polina <polina9130@gmail.com>
2022-07-26 15:18:29 +0300

    add dummy tests

--- C# Advanced/C# OOP/Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
+++ C# Advanced/C# OOP/Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace Skeleton.Tests
 {
@@ -6,9 +7,50 @@ namespace Skeleton.Tests
     public class DummyTests
     {
         [Test]
-        public void Test1()
+        public void ShouldLoosesHealthIfIsAttack()
         {
+            //Arrange
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+            
+            //Act
+            dummy.TakeAttack(2);
+
+            //Assert
+            Assert.That(dummy.Health, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void ThrowExceptionIfIsDeadButIsAttacked()
+        { 
+            //Arrenge
+            Dummy dummy = new Dummy(0, 10);
+            
+            //Act & Assert
+            Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(2));
+        }
+
+        [Test]
+        public void ShouldCanGiveXPIfIsDead()
+        {
+            //Arrenge
+            Dummy dummy = new Dummy(0, 10);
+
+            //Act & Assert
+            Assert.That(dummy.GiveExperience(), Is.EqualTo(10));
+            
+        }
+
+        [Test]
+        public void ShouldCantGiveXPIfIsAlive()
+        {
+            //Arrenge
+            Dummy dummy = new Dummy(10, 10);
+            
+            //Act & Assert
+            Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
 
         }
+        
     }
 }
