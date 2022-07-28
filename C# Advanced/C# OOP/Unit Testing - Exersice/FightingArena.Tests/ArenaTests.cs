d24644dd70be22b025d811dd8b255c4a0d42440a
Polina <polina9130@gmail.com>
2022-07-28 15:14:14 +0300

    add fight tests

--- C# Advanced/C# OOP/Unit Testing - Exersice/FightingArena.Tests/ArenaTests.cs
+++ C# Advanced/C# OOP/Unit Testing - Exersice/FightingArena.Tests/ArenaTests.cs
@@ -1,10 +1,110 @@
 namespace FightingArena.Tests
 {
     using NUnit.Framework;
+    using System;
+    using System.Linq;
 
     [TestFixture]
     public class ArenaTests
     {
+        [Test]
+        public void CountShouldReturnCorrectData()
+        {
+            Arena arena = new Arena();
+            Warrior firstWarrior = new Warrior("Polina", 10, 60);
+            Warrior secondWarrior = new Warrior("Maria", 20, 50);
+            arena.Enroll(firstWarrior);
+            arena.Enroll(secondWarrior);
 
+            int dataCount = arena.Count;
+            int expectedCount = 2;
+
+            Assert.AreEqual(expectedCount, dataCount);
+        }
+
+        [Test]
+        public void ShouldThrowErrorIfTryEnrollSamePerson()
+        {
+            Arena arena = new Arena();
+            Warrior firstWarrior = new Warrior("Polina", 10, 60);
+            arena.Enroll(firstWarrior);
+            Warrior secondWarrior = new Warrior("Polina", 20, 30);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                arena.Enroll(secondWarrior);
+            }, "Warrior is already enrolled for the fights!");
+
+        }
+
+        [Test]
+        public void EnrollReturnCorrectAdding()
+        {
+            Arena arena = new Arena();
+            Warrior firstWarrior = new Warrior("Polina", 10, 60);
+            Warrior secondWarrior = new Warrior("Maria", 10, 60);
+            arena.Enroll(firstWarrior);
+            arena.Enroll(secondWarrior);
+
+            int dataCount = arena.Count;
+            int expectedCount = 2;
+
+            Assert.AreEqual(expectedCount, dataCount);
+        }
+
+
+        [TestCase("Polina", "Maria")]
+        public void FightShouldAttackSuccessful(string attacker, string defender)
+        {
+            Arena arena = new Arena();
+            Warrior firstWarrior = new Warrior("Polina", 10, 60);
+            Warrior secondWarrior = new Warrior("Maria", 10, 60);
+            arena.Enroll(firstWarrior);
+            arena.Enroll(secondWarrior);
+
+            Warrior attackerWarrior = arena.Warriors.FirstOrDefault(x => x.Name == attacker);
+            Warrior defenderWarrior = arena.Warriors.FirstOrDefault(x => x.Name == defender);
+            attackerWarrior.Attack(defenderWarrior);
+
+            string dataAttWarior = attackerWarrior.Name;
+            string expectedAttWarior = "Polina";
+
+            string dataDefendedWarr = defenderWarrior.Name;
+            string expectedDefendedWarior = "Maria";
+
+            Assert.AreEqual(expectedAttWarior, dataAttWarior);
+            Assert.AreEqual(expectedDefendedWarior, dataDefendedWarr);
+
+        }
+
+        [TestCase("Poli", "Maria")]
+        public void FightShouldAThrowExceptionIfCantFindAttacker(string attacker, string defender)
+        {
+            Arena arena = new Arena();
+            Warrior firstWarrior = new Warrior("Polina", 10, 60);
+            Warrior secondWarrior = new Warrior("Maria", 10, 60);
+            arena.Enroll(firstWarrior);
+            arena.Enroll(secondWarrior);
+
+
+            Assert.That(() => arena.Fight(attacker, defender), Throws.InvalidOperationException.With.Message.EqualTo($"There is no fighter with name {attacker} enrolled for the fights!"));
+
+
+        }
+
+        [TestCase("Polina", "Mari")]
+        public void FightShouldAThrowExceptionIfCantFindDefender(string attacker, string defender)
+        {
+            Arena arena = new Arena();
+            Warrior firstWarrior = new Warrior("Polina", 10, 60);
+            Warrior secondWarrior = new Warrior("Maria", 10, 60);
+            arena.Enroll(firstWarrior);
+            arena.Enroll(secondWarrior);
+
+
+            Assert.That(() => arena.Fight(attacker, defender), Throws.InvalidOperationException.With.Message.EqualTo($"There is no fighter with name {defender} enrolled for the fights!"));
+
+
+        }
     }
 }
--- C# Advanced/C# OOP/Unit Testing - Exersice/FightingArena.Tests/WarriorTests.cs
+++ C# Advanced/C# OOP/Unit Testing - Exersice/FightingArena.Tests/WarriorTests.cs
@@ -1,10 +1,170 @@
 ﻿namespace FightingArena.Tests
 {
     using NUnit.Framework;
+    using System;
 
     [TestFixture]
     public class WarriorTests
     {
+        [Test]
+        public void ShouldCtorReturnCorrectData()
+        {
+            Warrior warrior = new Warrior("Polina", 10, 50);
+            string dataName = warrior.Name;
+            string expectedName = "Polina";
 
+            int dataDamage = warrior.Damage;
+            int expectedDamage = 10;
+
+            int dataXP = warrior.HP;
+            int expectedXP = 50;
+
+            Assert.AreEqual(expectedName, dataName);
+            Assert.AreEqual(expectedDamage, dataDamage);
+            Assert.AreEqual(expectedXP, dataXP);
+        }
+
+        [TestCase("", 10, 50)]
+        [TestCase(null, 10, 50)]
+        public void ShouldPropNameThrowErrorForNullOrEmpty(string name, int damage, int xp)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior warrior = new Warrior(name, damage, xp);
+            }, "Name should not be empty or whitespace!");
+        }
+
+        [TestCase("Polina", 10, 50)]
+        public void ShouldPropNameReturnCorrectData(string name, int damage, int xp)
+        {
+            Warrior warrior = new Warrior(name, damage, xp);
+            string dataName = warrior.Name;
+            string expectedName = "Polina";
+
+            Assert.AreEqual(expectedName, dataName);
+        }
+
+        [TestCase("Polina", -10, 50)]
+        [TestCase("Polina", 0, 50)]
+        public void ShouldPropDamageThrowErrorForNegativeOrZeroNumber(string name, int damage, int xp)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior warrior = new Warrior(name, damage, xp);
+            }, "Damage value should be positive!");
+        }
+
+        [TestCase("Polina", 10, 50)]
+        public void ShouldPropDamageReturnCorrectData(string name, int damage, int xp)
+        {
+            Warrior warrior = new Warrior(name, damage, xp);
+            int dataDamage = warrior.Damage;
+            int expectedDamage = 10;
+
+            Assert.AreEqual(expectedDamage, dataDamage);
+        }
+
+        [TestCase("Polina", 10, -50)]
+        public void ShouldPropXPThrowErrorForNegativeNumber(string name, int damage, int xp)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior warrior = new Warrior(name, damage, xp);
+            }, "HP should not be negative!");
+        }
+
+        [TestCase("Polina", 10, 50)]
+        public void ShouldPropXPReturnCorrectData(string name, int damage, int xp)
+        {
+            Warrior warrior = new Warrior(name, damage, xp);
+            int dataXP = warrior.HP;
+            int expectedXP = 50;
+
+            Assert.AreEqual(expectedXP, dataXP);
+        }
+
+        [TestCase("Maria", 20, 30)]
+        [TestCase("Maria", 30, 10)]
+
+        public void AttackShouldThrowExrrorIfITryDownXPUnder30(string name, int damage, int xp)
+        {
+            Warrior firstWarrior = new Warrior("Polina", 10, 50);
+            Warrior secondWarrior = new Warrior(name, damage, xp);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                secondWarrior.Attack(firstWarrior);
+            }, "Your HP is too low in order to attack other warriors!");
+            
+        }
+
+        [TestCase("Maria", 20, 30)]
+        [TestCase("Maria", 30, 10)]
+
+        public void AttackShouldThrowExrrorIfTryDownXPUnder30(string name, int damage, int xp)
+        {
+            Warrior firstWarrior = new Warrior("Polina", 10, 50);
+            Warrior secondWarrior = new Warrior(name, damage, xp);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                firstWarrior.Attack(secondWarrior);
+            }, $"Enemy HP must be greater than 30 in order to attack him!");
+
+        }
+
+        [TestCase("Maria", 20, 50)]
+
+        public void AttackShouldThrowExrrorIfDamageIsBiggerThanXP(string name, int damage, int xp)
+        {
+            Warrior firstWarrior = new Warrior("Polina", 60, 50);
+            Warrior secondWarrior = new Warrior(name, damage, xp);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                secondWarrior.Attack(firstWarrior);
+            }, "You are trying to attack too strong enemy");
+
+        }
+
+        [Test]
+        public void AttackShouldReturnCorrectDataWhenWasAttack()
+        {
+            Warrior firstWarrior = new Warrior("Polina", 10, 60);
+            Warrior secondWarrior = new Warrior("Maria", 20, 50);
+            secondWarrior.Attack(firstWarrior);
+
+            int dataXP = firstWarrior.HP;
+            int expectedXP = 40;
+
+            Assert.AreEqual(expectedXP, dataXP);
+
+        }
+
+        [Test]
+        public void AttackShouldReturnZeroIfAttackIsBigger()
+        {
+            Warrior firstWarrior = new Warrior("Polina", 10, 60);
+            Warrior secondWarrior = new Warrior("Maria", 70, 50);
+            secondWarrior.Attack(firstWarrior);
+
+            int dataXP = firstWarrior.HP;
+            int expectedXP = 0;
+
+            Assert.AreEqual(expectedXP, dataXP);
+        }
+
+        [Test]
+        public void AttackShouldReturnCorrectDataWhenAttack()
+        {
+            Warrior firstWarrior = new Warrior("Polina", 10, 60);
+            Warrior secondWarrior = new Warrior("Maria", 10, 50);
+            secondWarrior.Attack(firstWarrior);
+
+            int dataXP = firstWarrior.HP;
+            int expectedXP = 50;
+
+            Assert.AreEqual(expectedXP, dataXP);
+        }
     }
 }
