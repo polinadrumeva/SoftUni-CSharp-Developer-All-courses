4e3be252bd2b702e6e7e0fb47f8686ef5dd941e5
Polina <polina9130@gmail.com>
2022-08-01 10:27:34 +0300

    add changes

---
+++ C# Advanced/C# OOP/Mocking and Test Driven Development - Lab/FakeAxeAndDummy1/FakeAxeAndDummy1/IWeapon.cs
@@ -0,0 +1,10 @@
+﻿namespace FakeAxeAndDummy1
+{
+    public interface IWeapon
+    {
+        int AttackPoints { get; }
+        int DurabilityPoints { get; }
+        public void Attack(ITarget target);
+
+    }
+}
