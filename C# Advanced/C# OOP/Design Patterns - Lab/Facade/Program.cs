a7126bcdd2b2ddd2474c93ba2b312d64ec246a14
Polina <polina9130@gmail.com>
2022-08-02 16:28:46 +0300

    add design patterns sln

---
+++ C# Advanced/C# OOP/Design Patterns - Lab/Facade/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Facade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
