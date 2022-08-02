507eee2adf94bdb111aa9bea92386318e1fd6eaf
Polina <polina9130@gmail.com>
2022-08-02 16:50:22 +0300

    add singleton

---
+++ C# Advanced/C# OOP/Design Patterns - Lab/Singleton/SingletonDataContainer.cs
@@ -0,0 +1,31 @@
+﻿namespace Singleton
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class SingletonDataContainer : ISingletonContainer
+    {
+        private Dictionary<string, int> capitals = new Dictionary<string, int>();
+
+        public SingletonDataContainer()
+        {
+            System.Console.WriteLine("Initializing singleton object");
+
+            var elements = File.ReadAllLines("capitals.txt");
+            for (int i = 0; i < elements.Length; i+=2)
+            {
+                capitals.Add(elements[i], int.Parse(elements[i + 1]));
+            }
+        }
+
+        public int GetPopulation(string name)
+        {
+            
+            return capitals[name];
+        }
+
+        private static SingletonDataContainer instance = new SingletonDataContainer();
+
+        public static SingletonDataContainer Instance => instance;
+    }
+}
---
+++ C# Advanced/C# OOP/Design Patterns - Lab/Singleton/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace Singleton
+{
+    using System;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = SingletonDataContainer.Instance;
+            var db1 = SingletonDataContainer.Instance;
+            var db2 = SingletonDataContainer.Instance;
+            var db3 = SingletonDataContainer.Instance;
+
+
+        }
+    }
+}
