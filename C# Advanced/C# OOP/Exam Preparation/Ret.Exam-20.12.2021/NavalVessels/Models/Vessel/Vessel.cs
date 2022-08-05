65efa1fe7e7a64f563ecbb165c1bd72171901ac0
Polina <polina9130@gmail.com>
2022-08-05 17:34:12 +0300

    add structure

---
+++ C# Advanced/C# OOP/Exam Preparation/Ret.Exam-20.12.2021/NavalVessels/Models/Vessel/Vessel.cs
@@ -0,0 +1,109 @@
+﻿namespace NavalVessels.Models.Vessel
+{
+    using NavalVessels.Models.Contracts;
+    using NavalVessels.Utilities.Messages;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+
+    public abstract class Vessel : IVessel
+    {
+        private string name;
+        private ICaptain captain;
+        private double mainWeaponCaliber;
+        private double speed;
+        private List<string> targets;
+        private const double defaultArmor = 0;
+
+        public string Name 
+        {
+            get
+            { 
+            return this.name;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(string.Format(ExceptionMessages.InvalidVesselName));
+                }
+
+                this.name = value;
+            }
+        }
+
+        public ICaptain Captain
+        {
+            get
+            {
+                return this.captain;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new NullReferenceException(string.Format(ExceptionMessages.InvalidCaptainToVessel));
+                }
+
+                this.captain = value;
+            }
+        }
+        public double ArmorThickness { get;  set; }
+
+        public double MainWeaponCaliber { get; protected set; }
+
+        public double Speed { get; protected set; }
+
+        public ICollection<string> Targets { get; private set; }
+
+        public Vessel(string name, double mainWeaponCaliber, double speed, double armorThickness)
+        {
+            this.Name = name;
+            this.MainWeaponCaliber = mainWeaponCaliber;
+            this.Speed = speed; 
+            this.ArmorThickness = armorThickness;
+            this.Targets = new List<string>();
+        }
+        public void Attack(IVessel target)
+        {
+            if (target == null)
+            {
+                throw new NullReferenceException(ExceptionMessages.InvalidTarget);
+            }
+
+            target.ArmorThickness -= this.MainWeaponCaliber;
+            if (target.ArmorThickness < 0)
+            {
+                target.ArmorThickness = 0;
+            }
+            this.Targets.Add(target.Name);
+        }
+
+        public virtual void RepairVessel()
+        {
+            this.ArmorThickness = defaultArmor;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($" - {this.Name}");
+            sb.AppendLine($"*Type: {this.GetType().Name}");
+            sb.AppendLine($"*Armor thickness: {this.ArmorThickness}");
+            sb.AppendLine($"*Main weapon caliber: {this.MainWeaponCaliber}");
+            sb.AppendLine($"*Speed: {this.Speed} knots");
+            if (this.Targets.Count == 0)
+            {
+                sb.AppendLine($"*Targets: None");
+            }
+            else
+            {
+                sb.AppendLine($"*Targets: {string.Join(", ", this.Targets)}");
+            }
+            
+            
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
