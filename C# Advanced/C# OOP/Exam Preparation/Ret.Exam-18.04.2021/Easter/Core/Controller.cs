ea0cca05e19ede0a0c9a568ba8f22bfd59dae9b6
Polinadrumeva <polina9230@gmail.com>
2022-08-11 14:17:45 +0300

    add part of business logic

---
+++ C# Advanced/C# OOP/Exam Preparation/Ret.Exam-18.04.2021/Easter/Core/Controller.cs
@@ -0,0 +1,75 @@
+﻿namespace Easter.Core
+{
+    using Easter.Core.Contracts;
+    using Easter.Models.Bunnies;
+    using Easter.Models.Bunnies.Contracts;
+    using Easter.Models.Dyes;
+    using Easter.Models.Eggs;
+    using Easter.Models.Eggs.Contracts;
+    using Easter.Repositories;
+    using Easter.Utilities.Messages;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+
+    public class Controller : IController
+    {
+        private BunnyRepository bunnies;
+        private EggRepository eggs;
+
+        public Controller()
+        {
+            this.bunnies = new BunnyRepository();
+            this.eggs = new EggRepository();
+        }
+        public string AddBunny(string bunnyType, string bunnyName)
+        {
+            IBunny bunny;
+            if (bunnyType == "HappyBunny")
+            {
+                bunny = new HappyBunny(bunnyName);
+            }
+            else if (bunnyType == "SleepyBunny")
+            {
+                bunny = new SleepyBunny(bunnyName);
+            }
+            else
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.InvalidBunnyType));
+            }
+
+            this.bunnies.Add(bunny);
+            return String.Format(OutputMessages.BunnyAdded, bunnyType, bunnyName);
+        }
+
+        public string AddDyeToBunny(string bunnyName, int power)
+        {
+            Dye dye = new Dye(power);
+            IBunny bunny = this.bunnies.FindByName(bunnyName);
+            if (bunny == null)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.InexistentBunny));
+            }
+
+            return String.Format(OutputMessages.DyeAdded, power, bunnyName);
+        }
+
+        public string AddEgg(string eggName, int energyRequired)
+        {
+            IEgg egg = new Egg(eggName, energyRequired);
+            this.eggs.Add(egg);
+            return String.Format(OutputMessages.EggAdded, eggName);
+        }
+
+        public string ColorEgg(string eggName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Report()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
