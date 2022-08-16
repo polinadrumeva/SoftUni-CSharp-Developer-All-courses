a748abfb0658c6cede436c9711934ffa5ff37bbc
Polina <polina9130@gmail.com>
2022-08-16 17:06:42 +0300

    add tests

--- C# Advanced/C# OOP/Exam Preparation/Ret.Exam-20.12.2021/BookSkeleton/Book.Tests/Tests.cs
+++ C# Advanced/C# OOP/Exam Preparation/Ret.Exam-20.12.2021/BookSkeleton/Book.Tests/Tests.cs
@@ -6,5 +6,41 @@
 
     public class Tests
     {
+        [Test]
+        public void ConstructorReturnCorrectData()
+        {
+            Book book = new Book("Ana Karenina", "Tolstoi");
+            string actualName = book.BookName;
+            string expectedName = "Ana Karenina";
+
+            string actualAuthor = book.Author;
+            string expectedAuthor = "Tolstoi";
+
+            Assert.AreEqual(expectedName, actualName);
+            Assert.AreEqual(expectedAuthor, actualAuthor);
+
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void NameShouldThrowExceptionIfIsNullOrEmpty(string name)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Book book = new Book(name, "Tolstoi");
+            }, $"Invalid {nameof(name)}!");
+
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void AuthorShouldThrowExceptionIfIsNullOrEmpty(string author)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Book book = new Book("Ana Karenina", author);
+            }, $"Invalid {nameof(author)}!");
+
+        }
     }
 }
