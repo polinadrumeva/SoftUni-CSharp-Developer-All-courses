d8627c4e6f07290ca38c5d2e486ceac450c10258
Polina <polina9130@gmail.com>
2022-08-17 16:02:47 +0300

    add tests

--- C# Advanced/C# OOP/Exam Preparation/Ret.Exam-20.12.2021/BookSkeleton/Book.Tests/Tests.cs
+++ C# Advanced/C# OOP/Exam Preparation/Ret.Exam-20.12.2021/BookSkeleton/Book.Tests/Tests.cs
@@ -42,5 +42,84 @@
             }, $"Invalid {nameof(author)}!");
 
         }
+
+        [Test]
+        public void AddFootnoteShouldThrowExceptionIfAlreadyExist()
+        {
+            Book book = new Book("Ana Karenina", "Tolstoi");
+            book.AddFootnote(1, "Very good start to the book");
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                book.AddFootnote(1, "Beautiful heroine");
+            }, "Footnote already exists!");
+
+        }
+
+        [Test]
+        public void AddFootnoteShouldReturnCorrectData()
+        {
+            Book book = new Book("Ana Karenina", "Tolstoi");
+            book.AddFootnote(1, "Very good start to the book");
+            book.AddFootnote(2, "Beautiful heroine");
+
+            int actualCount = book.FootnoteCount;
+            int expectedCount = 2;
+
+            Assert.AreEqual(expectedCount, actualCount);
+
+        }
+
+        [Test]
+        public void FindFootnoteShouldThrowExceptionIfNotExist()
+        {
+            Book book = new Book("Ana Karenina", "Tolstoi");
+            book.AddFootnote(1, "Very good start to the book");
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                book.FindFootnote(3);
+            }, "Footnote doesn't exists!");
+
+        }
+
+        [Test]
+        public void FindFootnoteShouldReturnCorrectData()
+        {
+            Book book = new Book("Ana Karenina", "Tolstoi");
+            book.AddFootnote(1, "Very good start to the book");
+            string actualData = book.FindFootnote(1);
+            string expectedData = "Footnote #1: Very good start to the book";
+
+            Assert.AreEqual(expectedData, actualData);
+
+        }
+
+        [Test]
+        public void AlterFootnoteShouldThrowExceptionIfNotExist()
+        {
+            Book book = new Book("Ana Karenina", "Tolstoi");
+            book.AddFootnote(1, "Very good start to the book");
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                book.AlterFootnote(3, "Heroine is very intersting personage.");
+            }, "Footnote doesn't exists!");
+
+        }
+
+        [Test]
+        public void AlterFootnoteShouldReturnCorrectData()
+        {
+            Book book = new Book("Ana Karenina", "Tolstoi");
+            book.AddFootnote(1, "Very good start to the book");
+            book.AlterFootnote(1, "Heroine is very intersting personage.");
+
+            string actualData = book.FindFootnote(1);
+            string expectedData = "Footnote #1: Heroine is very intersting personage.";
+
+            Assert.AreEqual(expectedData, actualData);
+
+        }
     }
 }
