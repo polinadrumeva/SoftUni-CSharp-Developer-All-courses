57a96a5894ee789d0f039ddc038dc127e4971799
Polina <polina9130@gmail.com>
2022-08-17 17:42:43 +0300

    add structure

---
+++ C# Advanced/C# OOP/Exam Preparation/Exam-12.12.2020/Bakery/Models/Tables/Table.cs
@@ -0,0 +1,106 @@
+﻿using Bakery.Models.BakedFoods.Contracts;
+using Bakery.Models.Drinks.Contracts;
+using Bakery.Models.Tables.Contracts;
+using Bakery.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bakery.Models.Tables
+{
+    public abstract class Table : ITable
+    {
+        private readonly IList<IBakedFood> foodOrders;
+        private readonly IList<IDrink> drinkOrders;
+
+        private int tableNumber;
+        private int capacity;
+        private int numberOfPeople;
+        private decimal pricePerPerson;
+        private bool isReserved;
+        private decimal price;
+
+        public IReadOnlyCollection<IBakedFood> FoodOrders => (List<IBakedFood>)this.foodOrders;
+        public IReadOnlyCollection<IDrink> DrinkOrders => (List<IDrink>)this.drinkOrders;
+        public int TableNumber { get; private set; }
+
+        public int Capacity
+        {
+            get
+            {
+                return this.capacity;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidTableCapacity));
+                }
+
+                this.capacity = value;
+            }
+        }
+
+        public int NumberOfPeople
+        {
+            get
+            {
+                return this.numberOfPeople;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidNumberOfPeople));
+                }
+
+                this.numberOfPeople = value;
+            }
+        }
+
+        public decimal PricePerPerson { get; private set; }
+
+        public bool IsReserved { get; private set; }
+
+        public decimal Price => this.PricePerPerson * this.NumberOfPeople;
+
+        public Table(int tableNumber, int capacity, decimal pricePerPerson)
+        {
+            this.TableNumber = tableNumber;
+            this.Capacity = capacity;
+            this.PricePerPerson = pricePerPerson;
+
+            this.foodOrders = new List<IBakedFood>();
+            this.drinkOrders = new List<IDrink>();
+        }
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetBill()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFreeTableInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OrderDrink(IDrink drink)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OrderFood(IBakedFood food)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reserve(int numberOfPeople)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
