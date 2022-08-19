cf700501aeae4484d47e588ad79fe6976c29993c
Polina <polina9130@gmail.com>
2022-08-19 13:30:53 +0300

    add unit tests

--- C# Advanced/C# OOP/Exam Preparation/Ret.Exam-22.08.2020/TheRace.Tests/RaceEntryTests.cs
+++ C# Advanced/C# OOP/Exam Preparation/Ret.Exam-22.08.2020/TheRace.Tests/RaceEntryTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 using TheRace;
 
 namespace TheRace.Tests
@@ -6,15 +7,88 @@ namespace TheRace.Tests
     public class RaceEntryTests
     {
 
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void NameDriverShouldThrowExceptionIfValueIsNull()
         {
+            UnitCar car = new UnitCar("Fiat", 100, 200);
+           
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                UnitDriver driver = new UnitDriver(null, car);
+            }, "Name cannot be null!");
+        }
+
+
+        [Test]
+        public void AddDriverShouldThrowExceptionIfDriverIsNull()
+        {
+            UnitDriver driver = null;
+            RaceEntry entry = new RaceEntry();
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                entry.AddDriver(driver);
+            }, "Driver cannot be null.");
         }
 
         [Test]
-        public void TestOne()
+        public void AddDriverShouldThrowExceptionIfDriverAlreadyExist()
         {
-            Assert.Pass();
+            UnitCar car = new UnitCar("Fiat", 100, 200);
+            UnitDriver driver = new UnitDriver("Polina", car);
+            RaceEntry entry = new RaceEntry();
+            entry.AddDriver(driver);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                entry.AddDriver(driver);
+            }, "Driver Polina is already added.");
+        }
+
+        [Test]
+        public void AddDriverShouldReturnCorrectData()
+        {
+            UnitCar car = new UnitCar("Fiat", 100, 200);
+            UnitDriver driver = new UnitDriver("Polina", car);
+            RaceEntry entry = new RaceEntry();
+            string actual = entry.AddDriver(driver);
+            string expected = "Driver Polina added in race.";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CalculateShouldThrowExceptionIfNotHaveMinPartip()
+        {
+            UnitCar car = new UnitCar("Fiat", 100, 200);
+            UnitDriver driver = new UnitDriver("Polina", car);
+            RaceEntry entry = new RaceEntry();
+            entry.AddDriver(driver);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                entry.CalculateAverageHorsePower();
+            }, "The race cannot start with less than 2 participants.");
+        }
+
+        [Test]
+        public void CalculateShouldReturnCorrectData()
+        {
+            UnitCar car = new UnitCar("Fiat", 50, 200);
+            UnitCar car1 = new UnitCar("Opel", 200, 200);
+            UnitCar car2 = new UnitCar("Moskvich", 50, 200);
+            UnitDriver driver = new UnitDriver("Polina", car);
+            UnitDriver driver1 = new UnitDriver("Maria", car1);
+            UnitDriver driver2 = new UnitDriver("Emo", car2);
+            RaceEntry entry = new RaceEntry();
+            entry.AddDriver(driver);
+            entry.AddDriver(driver1);
+            entry.AddDriver(driver2);
+
+            double actualAverage = entry.CalculateAverageHorsePower();
+            double expectedAverage = 100;
+
+            Assert.AreEqual(expectedAverage, actualAverage);
         }
     }
 }
