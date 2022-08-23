ac5b1a68c167cded44467ce2afe51fe9958b126b
Polina <polina9130@gmail.com>
2022-08-23 15:00:04 +0300

    add composite pattern

---
+++ C# Advanced/C# OOP/Design Pattern - Exercise/Composite/GiftBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite
+{
+    public abstract class GiftBase
+    {
+        protected string name;
+        protected decimal price;
+
+        public GiftBase(string name, decimal price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public abstract decimal CalculatePrice();
+    }
+}
---
+++ C# Advanced/C# OOP/Design Pattern - Exercise/Composite/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Composite
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var phone = new SingleGift("Samsung", 300);
+            phone.CalculatePrice();
+            Console.WriteLine();
+
+            var rootBox = new CompositeGift("RootBox", 0);
+            var truck = new SingleGift("Truck", 30);
+            var train = new SingleGift("Train", 10);
+            rootBox.Add(train);
+            rootBox.Add(truck);
+
+            Console.WriteLine($"Total price of this composite present is: {rootBox.CalculatePrice()} $");
+
+        }
+    }
+}
