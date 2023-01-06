7af6415c721e2de5d99860b05d80a9ead35027e5
Polina <polina9130@gmail.com>
2023-01-06 17:05:34 +0200

    add changes

--- C# DB/MS SQL/CRUD - Lab/CRUD - query.sql
+++ C# DB/MS SQL/CRUD - Lab/CRUD - query.sql
@@ -4,3 +4,6 @@ SELECT * FROM Countries
 
 SELECT * FROM Countries WHERE CountryCode = 'AD'
 
+SELECT Capital, CountryName, Population FROM Countries
+
+SELECT Capital AS C, CountryName, Population FROM Countries
--- C# DB/MS SQL/CRUD - Lab/Problem Concatanation.sql
+++ C# DB/MS SQL/CRUD - Lab/Problem Concatanation.sql
@@ -1,3 +1,5 @@
 SELECT IsoCode, CountryName, Capital FROM Countries
 
 SELECT IsoCode,CountryName, Capital, Capital + ', ' + CountryName AS [Full] FROM Countries
+
+
