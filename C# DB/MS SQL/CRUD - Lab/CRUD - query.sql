df9b765b0abf4a184fa391835ae742490749b2c8
Polina <polina9130@gmail.com>
2023-01-09 17:30:46 +0200

    changes

--- C# DB/MS SQL/CRUD - Lab/CRUD - query.sql
+++ C# DB/MS SQL/CRUD - Lab/CRUD - query.sql
@@ -1,9 +1,29 @@
-SELECT Id FROM Mountains
+SELECT Id 
+	FROM Mountains
 
-SELECT * FROM Countries
+SELECT * 
+	FROM Countries
 
-SELECT * FROM Countries WHERE CountryCode = 'AD'
+SELECT * 
+	FROM Countries 
+	WHERE CountryCode = 'AD'
 
-SELECT Capital, CountryName, Population FROM Countries
+SELECT Capital, CountryName, Population 
+	FROM Countries
 
-SELECT Capital AS C, CountryName, Population FROM Countries
+SELECT Capital AS C, CountryName, Population
+	FROM Countries
+
+SELECT TOP(10) Capital AS C, CountryName, Population 
+	FROM Countries
+
+SELECT CountryName, Population
+	FROM Countries 
+	WHERE Population > 900000 AND 
+	Capital > 'D'
+
+SELECT CountryName, Population
+	FROM Countries 
+	WHERE Population > 900000 AND 
+	Capital > 'D'
+	ORDER BY CountryName DESC
