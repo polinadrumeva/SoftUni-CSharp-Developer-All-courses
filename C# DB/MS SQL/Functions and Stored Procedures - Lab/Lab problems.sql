c2119a685cf5c7510f53d39193ffc3848dd4a865
Polina <polina9130@gmail.com>
2023-02-03 12:53:24 +0200

    complete lab problems

--- C# DB/MS SQL/Functions and Stored Procedures - Lab/Lab problems.sql
+++ C# DB/MS SQL/Functions and Stored Procedures - Lab/Lab problems.sql
@@ -18,3 +18,24 @@ END
 
 SELECT FirstName, LastName, Salary, dbo.ufn_GetSalaryLevel(Salary) AS SalaryLevel
 	FROM Employees
+
+-- Problem: Employees with three projects 
+
+CREATE PROC	AddEmployee(@EmployeeID INT, @ProjectID INT)
+AS
+	DECLARE @EmployeeProjectCount INT = 
+		(SELECT COUNT(*) FROM EmployeesProjects
+		WHERE @EmployeeID = EmployeeID)
+
+	IF @EmployeeProjectCount >= 3
+		THROW 50001, 'The employee has 3 projects.', 1
+
+	INSERT INTO EmployeesProjects(EmployeeID, ProjectID)
+		VALUES (@EmployeeID, @ProjectID)
+
+GO
+
+EXEC AddEmployee 2, 3
+EXEC AddEmployee 2, 4
+EXEC AddEmployee 2, 5
+EXEC AddEmployee 2, 6
