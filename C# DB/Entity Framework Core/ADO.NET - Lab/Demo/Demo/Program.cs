8567e6b68779ec4de25a5155b36463db11d8e94d
Polinadrumeva <polina9230@gmail.com>
2023-03-03 14:26:28 +0200

    complete demo

--- C# DB/Entity Framework Core/ADO.NET - Lab/Demo/Demo/Program.cs
+++ C# DB/Entity Framework Core/ADO.NET - Lab/Demo/Demo/Program.cs
@@ -13,45 +13,43 @@ namespace Demo
             //connection.Open();
 
             //with using
-            using (var connection = new SqlConnection("Server=.;Integrated Security=true;Database=SoftUni"))
-            {
-                connection.Open();
+            //using (var connection = new SqlConnection("Server=.;Integrated Security=true;Database=SoftUni"))
+            //{
+            //    connection.Open();
 
-                //string query = "SELECT COUNT(*) FROM Employees";
-                //SqlCommand sqlCommand = new SqlCommand(query, connection);
+            //    //string query = "SELECT COUNT(*) FROM Employees";
+            //    //SqlCommand sqlCommand = new SqlCommand(query, connection);
 
-                string queryUpdate = "UPDATE Employees SET Salary = Salary + 0.12";
-                SqlCommand cmd = new SqlCommand(queryUpdate, connection);
-                var rowsaffected = cmd.ExecuteNonQuery();
-                Console.WriteLine(rowsaffected);
+            //    string queryUpdate = "UPDATE Employees SET Salary = Salary + 0.12";
+            //    SqlCommand cmd = new SqlCommand(queryUpdate, connection);
+            //    var rowsaffected = cmd.ExecuteNonQuery();
+            //    Console.WriteLine(rowsaffected);
 
-            }
+            //}
 
-            using (var connection = new SqlConnection("Server=.;Integrated Security=true;Database=SoftUni"))
-            {
-                connection.Open();
+            //using (var connection = new SqlConnection("Server=.;Integrated Security=true;Database=SoftUni"))
+            //{
+            //    connection.Open();
 
-                string queryUpdate = "SELECT COUNT(*) FROM Employees";
-                SqlCommand cmd = new SqlCommand(queryUpdate, connection);
-                var result = (int)cmd.ExecuteScalar();
-                Console.WriteLine(result);
+            //    string queryUpdate = "SELECT COUNT(*) FROM Employees";
+            //    SqlCommand cmd = new SqlCommand(queryUpdate, connection);
+            //    var result = (int)cmd.ExecuteScalar();
+            //    Console.WriteLine(result);
 
-            }
+            //}
 
-            using (var connection = new SqlConnection("Server=.;Integrated Security=true;Database=SoftUni"))
+            using (var connection = new SqlConnection("Server=.\\SQLEXPRESS;Integrated Security=true;Database=SoftUni"))
             {
                 connection.Open();
 
                 string queryUpdate = "SELECT * FROM Employees";
                 var cmd = new SqlCommand(queryUpdate, connection);
                 SqlDataReader result = cmd.ExecuteReader();
-                result.Read();
-                Console.WriteLine(result[1]);
-                result.Read();
-                Console.WriteLine(result[1]);
-                result.Read();
-                Console.WriteLine(result[1]);
-
+                while (result.Read())
+                {
+                    Console.WriteLine(result[1]);
+                }
+                
             }
         }
     }
