e9f55ad4c07e91266ff8d85f6c69ddca6425cf03
Polinadrumeva <polina9230@gmail.com>
2023-03-12 00:50:31 +0200

    complete MiniORM

---
+++ C# DB/Entity Framework Core/ORM Fundamentals - Exercise/MiniORM.App/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace MiniORM.App
+{
+    public static class Config
+    {
+        public const string ConnectionString =
+            @"Server=.\SQLEXPRESS;Database=MiniORM;Integrated Security=True;Encrypt=False;";
+    }
+}
