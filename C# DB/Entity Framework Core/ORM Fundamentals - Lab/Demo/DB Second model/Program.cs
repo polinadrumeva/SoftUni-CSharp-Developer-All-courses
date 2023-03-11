2af134356995c39228e76e3c451a9bdad45dde73
Polinadrumeva <polina9230@gmail.com>
2023-03-11 23:57:14 +0200

    complete demo

--- C# DB/Entity Framework Core/ORM Fundamentals - Lab/Demo/DB Second model/Program.cs
+++ C# DB/Entity Framework Core/ORM Fundamentals - Lab/Demo/DB Second model/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using CodeFirstmodel.Models;
+using System.Collections.Generic;
 
 namespace CodeFirstmodel
 {
@@ -10,6 +11,25 @@ namespace CodeFirstmodel
         {
             var db = new AppDbContext();
             db.Database.EnsureCreated();
+
+            db.Categories.Add(new Category
+            {
+                Title = "Sport",
+                News = new List<News>
+                {
+                    new News { Title = "CSKA spechli kupata",
+                        Content = "ЦСКА спечели купата на България",
+                        Comments = new List<Comment>
+                        {
+                            new Comment { Author = "Niki", Content = "Bravo momcheta" },
+                            new Comment { Author = "Stoyan", Content = "Samo Levski" }
+                        }
+                    }
+                }
+
+            });
+
+            db.SaveChanges();
 
         }
     }
