28a2ca370c9423e59abbab997ba44df61812e8c0
Polina <polina9130@gmail.com>
2023-03-13 17:21:56 +0200

    start exercise

---
+++ C# DB/Entity Framework Core/Entity Framework Introduction - Exercise/SoftUni/Models/EmployeeProject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUni.Models
+{
+    public class EmployeeProject
+    {
+        public int EmployeeID { get; set; }
+        public virtual Employee Employee { get; set; }         
+
+        public int ProjectID { get; set; }
+        public virtual Project Project { get; set; }
+    }
+}
