500719bd6fe46ba36ae0ac28c4ad72e5ade43a96
Polina <polina9130@gmail.com>
2023-03-29 17:37:44 +0300

    start exercise

---
+++ C# DB/Entity Framework Core/Entity Relations - Exercise/StudentSystem/Data/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public DateTime RegisteredOn { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
