2ba407b11057eaa6e11ee99eaffcaebe5f757e14
Polina <polina9130@gmail.com>
2023-04-04 17:42:26 +0300

    start StudentSystem

---
+++ C# DB/Entity Framework Core/Entity Relations - Exercise/P01_StudentSystem.Data.Models/Course.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Course
+    {
+        /*     o	CourseId
+               o	Name – up to 80 characters, unicode
+               o	Description – unicode, not required
+               o	StartDate
+               o	EndDate
+               o	Price
+
+        */
+        public Course()
+        {
+            this.Students = new HashSet<Student>();
+            this.Resources = new HashSet<Resource>();
+            this.Homeworks = new HashSet<Homework>();
+            this.StudentsCourses = new HashSet<StudentCourse>();
+        }
+
+        [Key]
+        public int CourseId { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+
+        public virtual ICollection<Resource> Resources { get; set; }
+
+        public virtual ICollection<Homework> Homeworks { get; set; }
+
+        public virtual ICollection<StudentCourse> StudentsCourses { get; set; }
+    }
+}
---
+++ C# DB/Entity Framework Core/Entity Relations - Exercise/P01_StudentSystem.Data.Models/Student.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Student
+    {
+        /* o	StudentId
+           o	Name – up to 100 characters, unicode
+           o	PhoneNumber – exactly 10 characters, not unicode, not required
+           o	RegisteredOn
+           o	Birthday – not required
+        */
+
+        public Student()
+        {
+            this.Courses = new HashSet<Course>();
+            this.Homeworks = new HashSet<Homework>();
+            this.StudentsCourses = new HashSet<StudentCourse>();
+        }
+
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(10)]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required]
+        public DateTime RegisteredOn { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public virtual ICollection<Course> Courses { get; set; }
+
+        public virtual ICollection<Homework> Homeworks { get; set; }
+
+        public virtual ICollection<StudentCourse> StudentsCourses { get; set; }
+    }
+}
