e42c70420deaeb37b746618ad85824efb31ff008
Polina <polina9130@gmail.com>
2023-04-05 17:54:45 +0300

    start SalesDatabase

---
+++ C# DB/Entity Framework Core/Entity Relations - Exercise/P03_SalesDatabase/Configurations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03_SalesDatabase
+{
+    public class Configurations
+    {
+        public static string ConfigurationAddress = "Server=.;Database=SalesDatabase;Integrated security=true";
+    }
+}
---
+++ C# DB/Entity Framework Core/Entity Relations - Exercise/P03_SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03_SalesDatabase.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext()
+        {
+            
+        }
+
+        public SalesContext(DbContextOptions options)
+            : base(options) 
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configurations.ConfigurationAddress);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
