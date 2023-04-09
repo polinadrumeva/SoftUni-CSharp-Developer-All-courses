e8728ce2e7cf5f11de8245dd3524a59965b19635
Polinadrumeva <polina9230@gmail.com>
2023-04-09 14:32:52 +0300

    add demo

---
+++ C# DB/Entity Framework Core/JSON Processing - Lab/Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Demo.Data.Models;
+using Demo.DTOs;
+using System.IO;
+using System.Text.Json;
+using static System.Net.WebRequestMethods;
+
+namespace Demo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var db = new SoftUniContext();
+
+            var employees = db.Employees.Select(e => new EmployeeDTO
+            {
+                FirstName = e.FirstName,    
+                LastName = e.LastName,
+                MiddleName = e.MiddleName,
+                FullName = $"{e.FirstName} {e.LastName}",
+                DepartmentName = e.Department.Name,
+                Salary = e.Salary
+            }).ToList();
+
+            var jsonFormat = JsonSerializer.Serialize(employees);
+            Console.WriteLine(jsonFormat);
+
+            var employeesFromJson = JsonSerializer.Deserialize<EmployeeDTO[]>(jsonFormat);
+        }
+    }
+}
