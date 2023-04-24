a411854833caea1cdf6b517f5619f8e9bfd60520
Polina <polina9130@gmail.com>
2023-04-24 15:33:25 +0300

    add resources

---
+++ C# DB/Entity Framework Core/XML Processing -Lab/Demo/DTOs/EmployeeDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DTOs
+{
+    public class EmployeeDTO
+    {
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
---
+++ C# DB/Entity Framework Core/XML Processing -Lab/Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Demo.Data.Models;
+using Demo.DTOs;
+using System.IO;
+using System.Text.Json;
+using static System.Net.WebRequestMethods;
+
+namespace Demo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var db = new SoftUniContext();
+
+            
+        }
+    }
+}
