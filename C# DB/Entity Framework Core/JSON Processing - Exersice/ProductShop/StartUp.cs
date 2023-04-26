90b0164f18ffd86baed27bab6f7603e5b12d93c4
Polina <polina9130@gmail.com>
2023-04-26 17:05:26 +0300

    complete 05-09

--- C# DB/Entity Framework Core/JSON Processing - Exersice/ProductShop/StartUp.cs
+++ C# DB/Entity Framework Core/JSON Processing - Exersice/ProductShop/StartUp.cs
@@ -4,6 +4,8 @@ using Newtonsoft.Json;
 using ProductShop.Data;
 using ProductShop.DTOs.Import;
 using ProductShop.Models;
+using System.Diagnostics;
+using System.Xml.Linq;
 
 namespace ProductShop
 {
@@ -24,7 +26,7 @@ namespace ProductShop
             //string result = ImportCategoryProducts(db, inputJson);
             //Console.WriteLine(result);
 
-            var result = GetProductsInRange(db);
+            var result = GetUsersWithProducts(db);
             Console.WriteLine(result);
 
         }
@@ -159,8 +161,80 @@ namespace ProductShop
 
         //06. Export Sold Products
         public static string GetSoldProducts(ProductShopContext context)
-        { 
-            
+        {
+            /*Get all users who have at least 1 sold item with a buyer. Order them by the last name, then by the first name. Select the person's first and last name. For each of the sold products (products with buyers), select the product's name, price and the buyer's first and last name.*/
+
+            var users = context.Users.Where(u => u.ProductsSold.Any(x => x.Buyer != null))
+                                .OrderBy(u => u.LastName)
+                                .ThenBy(u => u.FirstName)
+                                .Select(u => new
+                                {
+                                    firstName = u.FirstName,
+                                    lastName = u.LastName,
+                                    soldProducts = u.ProductsSold.Where(x => x.Buyer != null).Select(x => new
+                                    {
+                                        name = x.Name,
+                                        price = x.Price,
+                                        buyerFirstName = x.Buyer.FirstName,
+                                        buyerLastName = x.Buyer.LastName
+                                    })
+                                }).AsNoTracking()
+                                .ToArray();
+
+            return JsonConvert.SerializeObject(users, Formatting.Indented);
+                                
+        }
+
+        //07. Export Categories By Products Count
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+            /*Get all categories. Order them in descending order by the category's products count. For each category select its name, the number of products, the average price of those products (rounded to the second digit after the decimal separator) and the total revenue (total price sum and rounded to the second digit after the decimal separator) of those products (regardless if they have a buyer or not).*/
+
+            var categories = context.Categories.OrderByDescending(x => x.CategoriesProducts.Count())
+                                    .Select(x => new
+                                        {
+                                        category = x.Name,
+                                        productsCount = x.CategoriesProducts.Count(),
+                                        averagePrice = Math.Round((double)(x.CategoriesProducts.Average(cp => cp.Product.Price)),2),
+                                        totalRevenue = Math.Round((double)(x.CategoriesProducts.Sum(cp => cp.Product.Price)), 2)
+                                    })
+                                    .AsNoTracking()
+                                    .ToArray();
+
+            return JsonConvert.SerializeObject(categories, Formatting.Indented);
+        }
+
+        //08. Export Users and Products
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+            /*Get all users who have at least 1 sold product with a buyer. Order them in descending order by the number of sold products to a buyer. Select only their first and last name and age and for each product – name and price. Ignore all null values.*/
+
+            var users = context.Users.Where(u => u.ProductsSold.Any(b => b.Buyer != null))
+                                .OrderByDescending(u => u.ProductsSold.Count())
+                                .Select(u => new 
+                                    {
+                                       firstName = u.FirstName,
+                                       lastName = u.LastName,
+                                       age = u.Age,
+                                       soldProducts = new
+                                       {
+                                           count = u.ProductsSold.Count(ps => ps.Buyer != null),
+                                           products = u.ProductsSold.Where(ps => ps.Buyer != null).Select(ps => new
+                                           {
+                                               name = ps.Name,
+                                               price = ps.Price
+                                           }).ToArray()
+                                       }
+                                    }).AsNoTracking() 
+                                    .ToArray();
+
+            var user = new
+            {
+                usersCount = users.Length,
+                users 
+            };
+
+            return JsonConvert.SerializeObject(user, Formatting.Indented, new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore});  
         }
     }
 }
