eba50fc40ac2b302a99e68dad952631cb0316c38
Polina <polina9130@gmail.com>
2023-05-03 17:17:05 +0300

    add demo

--- C# DB/Entity Framework Core/XML Processing -Lab/Demo/Data/SoftUniContext.cs
+++ C# DB/Entity Framework Core/XML Processing -Lab/Demo/Data/SoftUniContext.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using Demo.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace Demo.Data.Models
+namespace Demo.Data
 {
     public partial class SoftUniContext : DbContext
     {
@@ -26,8 +27,7 @@ namespace Demo.Data.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=SoftUni;Integrated Security=True;");
+                optionsBuilder.UseSqlServer("Server=.;Database=SoftUni;Integrated Security=True;");
             }
         }
 
--- C# DB/Entity Framework Core/XML Processing -Lab/Demo/Program.cs
+++ C# DB/Entity Framework Core/XML Processing -Lab/Demo/Program.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Demo.Data;
 using Demo.Data.Models;
 using Demo.DTOs;
+using Microsoft.EntityFrameworkCore;
 using System.IO;
-using System.Text.Json;
+using System.Xml.Linq;
 using static System.Net.WebRequestMethods;
 
 namespace Demo
@@ -14,7 +16,25 @@ namespace Demo
         {
             var db = new SoftUniContext();
 
-            
+            var employees = db.Employees.Where(e => e.DepartmentId == 3).Include(e => e.Department).ToList();
+
+            var doc = new XDocument();
+            var root = new XElement("employees");
+            string department = employees.FirstOrDefault().Department.Name;
+            root.Add(new XAttribute("department", department));
+
+            foreach (var employee in employees)
+            {
+                var empl = new XElement("employee");
+                empl.Add(new XElement("name", $"{employee.FirstName} {employee.LastName}"), 
+                         new XElement("salary", employee.Salary));
+
+                root.Add(empl);
+            }
+
+            doc.Add(root);
+            doc.Save("employees.xml");
+
         }
     }
 }
