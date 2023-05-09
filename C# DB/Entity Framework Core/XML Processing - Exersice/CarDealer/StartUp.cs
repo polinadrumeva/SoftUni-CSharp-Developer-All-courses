2d48df3e41647af6d1f69759b6a6ec1322998460
Polina <polina9130@gmail.com>
2023-05-09 17:25:13 +0300

    start problems car dealer

--- C# DB/Entity Framework Core/XML Processing - Exersice/CarDealer/StartUp.cs
+++ C# DB/Entity Framework Core/XML Processing - Exersice/CarDealer/StartUp.cs
@@ -1,10 +1,27 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
 
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var xmlRoot = new XmlRootAttribute("Suppliers");
+
+            var serializer = new XmlSerializer(typeof(ImportSupplierDTO[]), xmlRoot);
+
+            var reader = new StringReader(inputXml);
+            var suppliersDTO = (ImportSupplierDTO[])serializer.Deserialize(reader);
+
+            
         }
     }
 }
