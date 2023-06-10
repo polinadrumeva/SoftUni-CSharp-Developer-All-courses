2058632f6537061e893683a52594986adb97d991
Polinadrumeva <polina9230@gmail.com>
2023-06-11 00:56:51 +0300

    complete TextSplitter

---
+++ C# Web/ASP.NET Fundamentals/ASP.NET Core Introduction - Exercise/TextSplitterApp/Models/TextViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TextSplitterApp.Models
+{
+	public class TextViewModel
+	{
+		[Required]
+		[MinLength(2)]
+		[MaxLength(30)]
+		public string Text { get; set; } = null!;
+
+		public string SplitText { get; set; } = null!;
+    }
+}
