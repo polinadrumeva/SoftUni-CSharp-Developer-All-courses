50029fdc850b3c6db44e346d000d65ae22d7357e
Polina <polina9130@gmail.com>
2023-06-14 17:11:39 +0300

    start demo

---
+++ C# Web/ASP.NET Fundamentals/ASP.NET Identity/IdentityDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityDemo.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
