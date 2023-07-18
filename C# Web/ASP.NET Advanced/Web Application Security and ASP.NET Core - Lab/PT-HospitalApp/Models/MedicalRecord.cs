57faffc41269f0dbe2bbea68bf0b2e95051daa6d
Polina <polina9130@gmail.com>
2023-07-18 09:42:29 +0300

    complete demo

--- C# Web/ASP.NET Advanced/Web Application Security and ASP.NET Core - Lab/PT-HospitalApp/Models/MedicalRecord.cs
+++ C# Web/ASP.NET Advanced/Web Application Security and ASP.NET Core - Lab/PT-HospitalApp/Models/MedicalRecord.cs
@@ -4,7 +4,8 @@ namespace PT_HospitalApp.Models
 {
     public class MedicalRecord
     {
-        public int Id { get; set; }
+        // Solution: use Guid instead of int
+        public Guid Id { get; set; }
         public DateTime Date { get; set; }
         public string DoctorName { get; set; }
     }
